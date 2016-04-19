90efd6424811cdccd0e7fdaa22ecaea3a2e28f51
Junpeng Qiu <qjpchmail@gmail.com>
2016-04-18 23:49:11 -0400

    Update #11

--- Java/ContainerWithMostWater.java
+++ Java/ContainerWithMostWater.java
@@ -4,42 +4,17 @@ import java.util.ArrayList;
 
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        TreeMap<Integer, ArrayList<Integer>> bucket = new TreeMap<>();
-        for (int i = 0; i < height.length; i++) {
-            if (bucket.containsKey(height[i])) {
-                bucket.get(height[i]).add(i);
-            } else {
-                ArrayList<Integer> list = new ArrayList<>();
-                list.add(i);
-                bucket.put(height[i], list);
-            }
-        }
-        int maxAreaValue = 0;
-        boolean[] flags = new boolean[height.length];
         int low = 0, high = height.length - 1;
-        for (Map.Entry<Integer, ArrayList<Integer>> entry : bucket.entrySet()) {
-            int minValue = Integer.MAX_VALUE, maxValue = Integer.MIN_VALUE;
-            for (Integer i : entry.getValue()) {
-                if (i < minValue) {
-                    minValue = i;
-                }
-                if (i > maxValue) {
-                    maxValue = i;
-                }
-                flags[i] = true;
-            }
-            int area = entry.getKey() * Math.max(high - minValue, maxValue - low);
-            if (area > maxAreaValue) {
-                maxAreaValue = area;
-            }
-            while (low <= high && flags[low]) {
+        int maxValue = 0;
+        while (low < high) {
+            maxValue = Math.max(maxValue, Math.min(height[low], height[high]) * (high - low));
+            if (height[low] > height[high]) {
+                high--;
+            } else {
                 low++;
             }
-            while (low <= high && flags[high]) {
-                high--;
-            }
         }
-        return maxAreaValue;
+        return maxValue;
     }
 
 }
