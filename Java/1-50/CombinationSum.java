df8852085e556ef7885f6c74a7cebf315429e76a
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-19 18:44:26 -0400

    Simplify combination sum

--- Java/1-50/CombinationSum.java
+++ Java/1-50/CombinationSum.java
@@ -1,38 +1,23 @@
 public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < candidates.length; i++) {
-            List<List<Integer>> subRes = getCombinationSum(candidates, i, target, 0);
-            if (subRes != null) res.addAll(subRes);
-        }
-        return res;
+        return getCombinationSum(candidates, 0, target);
     }
-    public List<List<Integer>> getCombinationSum(int[] candidates, int low, int target, int depth) {
+    public List<List<Integer>> getCombinationSum(int[] candidates, int low, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        if (depth == 0) target -= candidates[low];
         if (target == 0) {
-            if (depth == 0) {
-                List<Integer> subRes = new ArrayList<Integer>();
-                subRes.add(candidates[low]);
-                res.add(subRes);
-            } else {
-                res.add(new ArrayList<Integer>());
-            }
+            res.add(new ArrayList<>());
             return res;
         }
         if (candidates[low] > target) return null;
         for (int i = low; i < candidates.length; i++) {
-            if (candidates[i] <= target) {
-                List<List<Integer>> combs = getCombinationSum(candidates, i, target - candidates[i], depth + 1);
-                if (combs != null) {
-                    for (List<Integer> lst : combs) {
-                        ArrayList<Integer> subRes = new ArrayList<>();
-                        if (depth == 0) subRes.add(candidates[low]);
-                        subRes.add(candidates[i]);
-                        subRes.addAll(lst);
-                        res.add(subRes);
-                    }
+            List<List<Integer>> combs = getCombinationSum(candidates, i, target - candidates[i]);
+            if (combs != null) {
+                for (List<Integer> lst : combs) {
+                    ArrayList<Integer> subRes = new ArrayList<>();
+                    subRes.add(candidates[i]);
+                    subRes.addAll(lst);
+                    res.add(subRes);
                 }
             }
         }
