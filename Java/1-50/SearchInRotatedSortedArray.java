69d42f2bbc884ab7ddb8a10eda95768f39e5fb8e
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-20 19:53:25 -0400

    Fix search in rorated sort array

--- Java/1-50/SearchInRotatedSortedArray.java
+++ Java/1-50/SearchInRotatedSortedArray.java
@@ -1,37 +1,27 @@
 public class SearchInsertPosition {
     public int search(int[] nums, int target) {
         if (nums.length == 0) return -1;
-        int pivot = findPivot(nums);
-        int low, high;
-        if (target >= nums[0] && target <= nums[pivot]) {
-            low = 0;
-            high = pivot;
-        } else if (pivot < nums.length - 1 && target >= nums[pivot + 1] &&
-                   target <= nums[nums.length - 1]) {
-            low = pivot + 1;
-            high = nums.length - 1;
-        } else {
-            return -1;
+        int low = 0;
+        int high = nums.length - 1;
+        int type = 0;
+        if (nums[0] > nums[nums.length - 1]) {
+            if (target >= nums[0]) type = 1;
+            else type = 2;
         }
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (target == nums[mid]) {
+            if (nums[mid] == target)
                 return mid;
-            } else if (target > nums[mid]) {
+            if (type == 1 && nums[mid] < nums[0]) {
+                high = mid - 1;
+            } else if (type == 2 && nums[mid] > nums[nums.length - 1]) {
+                low = mid + 1;
+            } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
         return -1;
-    }
-    public int findPivot(int[] nums) {
-        int prev = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < prev) {
-                return i - 1;
-            }
-        }
-        return nums.length - 1;
     }
 }
