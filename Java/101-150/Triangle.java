ebddd5d85101e72d4981742d4c97ba897622a392
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-21 17:02:54 -0400

    Add triangle

---
+++ Java/101-150/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            List<Integer> row = triangle.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                dp[j] = row.get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+}
