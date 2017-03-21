9d44edf817dd012fac11510460ec0b7c5f3b63a5
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-20 20:07:08 -0400

    Fix search in rotated sorted array 2

--- Java/51-100/SearchInRotatedSortedArray2.java
+++ Java/51-100/SearchInRotatedSortedArray2.java
@@ -1,44 +1,37 @@
 public class SearchInRotatedSortedArray2 {
     public boolean search(int[] nums, int target) {
         if (nums.length == 0) return false;
-        int pivotIndex = findPivotIndex(nums);
-        int low, high;
-        if (pivotIndex == nums.length) {
-            if (target < nums[0] || target > nums[nums.length - 1]) return false;
-            low = 0;
-            high = pivotIndex - 1;
-        } else {
-            if (target >= nums[0] && target <= nums[pivotIndex - 1]) {
-                low = 0;
-                high = pivotIndex - 1;
-            } else if (target >= nums[pivotIndex] && target <= nums[nums.length - 1]) {
-                low = pivotIndex;
-                high = nums.length - 1;
-            } else {
-                return false;
-            }
+        int low = 0;
+        int high = nums.length - 1;
+        if (target == nums[0]) return true;
+        if (nums[0] == nums[nums.length - 1]) {
+            while (low <= high && nums[low] == nums[0]) low++;
+            while (high >= low && nums[high] == nums[0]) high--;
+            if (low > high) return false;
+            if (low == high)
+                return nums[low] == target;
+        }
+        int type = 0;
+        int start = nums[low];
+        int end = nums[high];
+        if (nums[low] > nums[high]) {
+            if (target >= nums[low]) type = 1;
+            else if (target <= nums[high]) type = 2;
+            else return false;
         }
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
+            if (nums[mid] == target) return true;
+            if (type == 1 && nums[mid] <= end) {
                 high = mid - 1;
+            } else if (type == 2 && nums[mid] >= start) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return false;
-    }
-    public int findPivotIndex(int[] nums) {
-        int prev = nums[0] - 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= prev) {
-                prev = nums[i];
-            } else {
-                return i;
-            }
-        }
-        return nums.length;
     }
 }
