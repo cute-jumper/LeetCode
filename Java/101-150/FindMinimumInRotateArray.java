6db1ca84757545284525f243cd329159d1de6aae
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-22 17:46:56 -0400

    Add find minimum in rotated sorted array

---
+++ Java/101-150/FindMinimumInRotateArray.java
@@ -0,0 +1,18 @@
+public class FindMinimumInRotateArray {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        if (nums[low] <= nums[high]) {
+            return nums[low];
+        }
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[nums.length - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
