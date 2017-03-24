c2bf6104b29c496b545fa4b8db8bf0ca60216ddf
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-24 00:46:59 -0400

    Add contains duplicate

---
+++ Java/201-250/ContainsDuplicate.java
@@ -0,0 +1,9 @@
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!seen.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
