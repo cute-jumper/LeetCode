f8e058cca3d6caa40d65d624327dcc0cf4d3babe
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-24 13:59:01 -0400

    Add invert binary tree

---
+++ Java/201-250/InvertBinaryTree.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode right = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(right);
+        return root;
+    }
+}
