d4228ff63a93089d36522cb443a803ae6f992cf3
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-26 15:04:25 -0400

    Add bulls and cows

---
+++ Java/251-300/BullsAndCows.java
@@ -0,0 +1,20 @@
+public class BullsAndCows {
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+        int[] count = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            int s = secret.charAt(i) - '0';
+            int g = guess.charAt(i) - '0';
+            if (s == g) {
+                bulls++;
+                continue;
+            }
+            if (count[s] > 0) cows++;
+            if (count[g] < 0) cows++;
+            count[s]--;
+            count[g]++;
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
