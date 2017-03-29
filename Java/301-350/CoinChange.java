5f201cc5c5742273319da28bf8c605f28e09576a
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-29 17:20:51 -0400

    Add coin change

---
+++ Java/301-350/CoinChange.java
@@ -0,0 +1,16 @@
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int c : coins) {
+                if (i == c) dp[i] = 1;
+                else if (i > c && dp[i - c] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
