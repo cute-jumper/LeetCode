0b42515bf74ed962795975ee379b80310bc7579f
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-31 13:48:11 -0400

    Add find the difference

---
+++ Java/351-400/FindTheDifference.java
@@ -0,0 +1,12 @@
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (count[t.charAt(i) - 'a']-- == 0) return t.charAt(i);
+        }
+        return 0;
+    }
+}
