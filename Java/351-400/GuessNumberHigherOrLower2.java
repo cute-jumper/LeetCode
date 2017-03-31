7de2b9a6442276c2deb6153fe36bdaebbbe8cab7
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-30 23:03:24 -0400

    Add guess number higher or lower 2

---
+++ Java/351-400/GuessNumberHigherOrLower2.java
@@ -0,0 +1,20 @@
+public class GuessNumberHigherOrLower2 {
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][i] = 0;
+            dp[i][i + 1] = i + 1;
+        }
+        dp[n - 1][n - 1] = 0;
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j + i < n; j++) {
+                dp[j][j + i] = Integer.MAX_VALUE;
+                for (int k = 1; k < i; k++) {
+                    dp[j][j + i] = Math.min(dp[j][j + i],
+                                            j + k + 1 + Math.max(dp[j][j + k - 1], dp[j + k + 1][j + i]));
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
