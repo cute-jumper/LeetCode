a3ed5a114b0764caf446b6cb29e936ed86c67591
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-01 04:29:21 -0400

    Add fizzbuzz

---
+++ Java/401-450/FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            StringBuilder sb = new StringBuilder();
+            if (i % 3 == 0) sb.append("Fizz");
+            if (i % 5 == 0) sb.append("Buzz");
+            if (sb.length() == 0) sb.append(i);
+            res.add(sb.toString());
+        }
+        return res;
+    }
+}
