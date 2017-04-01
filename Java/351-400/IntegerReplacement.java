83d7154e4f1bdae5118c660310588e55134fe847
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-31 21:41:39 -0400

    Don't use long for integer replacement

--- Java/351-400/IntegerReplacement.java
+++ Java/351-400/IntegerReplacement.java
@@ -1,5 +1,13 @@
 public class IntegerReplacement {
     public int integerReplacement(int n) {
+        int count = 0;
+        while (n % 2 == 0) {
+            count++;
+            n /= 2;
+        }
+        if (n == 1) return count;
+        return count + 2 + Math.min(integerReplacement((n - 1) / 2 + 1),
+                                    integerReplacement((n - 1) / 2));
         // if (n < 3) return n - 1;
         // int old = n;
         // if (n % 2 == 1) n++;
@@ -14,17 +22,5 @@ public class IntegerReplacement {
         //     dp[n - 1] = 1 + dp[n - 2];
         // }
         // return dp[old - 1];
-        return countN(n);
-    }
-    public int countN(long n) {
-        int count = 0;
-        while (n % 2 == 0) {
-            count++;
-            n /= 2;
-        }
-        if (n == 1) return count;
-        int res = count + 1 + Math.min(countN(n + 1),
-                                       countN(n - 1));
-        return res;
     }
 }
