d3610c7491898833c00ba9555fa88d66dd982f6e
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-03 02:56:20 -0400

    Add add two numbers 2

---
+++ Java/401-450/AddTwoNumbers2.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class AddTwoNumbers2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Deque<ListNode> stack1 = new ArrayDeque<>();
+        Deque<ListNode> stack2 = new ArrayDeque<>();
+        ListNode p = l1;
+        while (p != null) {
+            stack1.push(p);
+            p = p.next;
+        }
+        p = l2;
+        while (p != null) {
+            stack2.push(p);
+            p = p.next;
+        }
+        int carry = 0;
+        ListNode vh = new ListNode(0);
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            int sum = carry + (stack1.isEmpty() ? 0 : stack1.pop().val);
+            sum += stack2.isEmpty() ? 0 : stack2.pop().val;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            node.next = vh.next;
+            vh.next = node;
+        }
+        if (carry == 1) {
+            ListNode node = new ListNode(1);
+            node.next = vh.next;
+            vh.next = node;
+        }
+        return vh.next;
+    }
+}
