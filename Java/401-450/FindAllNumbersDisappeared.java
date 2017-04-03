ce865356a6428cd7395e9308d69f0131ce9933ab
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-03 03:19:20 -0400

    Add find all numbers disappeared in an array

---
+++ Java/401-450/FindAllNumbersDisappeared.java
@@ -0,0 +1,13 @@
+public class FindAllNumbersDisappeared {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]);
+            nums[val - 1] = -1 * Math.abs(nums[val - 1]);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) res.add(i + 1);
+        }
+        return res;
+    }
+}
