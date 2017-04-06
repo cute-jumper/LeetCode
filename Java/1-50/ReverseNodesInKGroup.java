b9dfca3c040c07bb42eeddd47ff43e6dc45c4e48
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-06 17:17:57 -0400

    Add reverse nodes in k-Group

---
+++ Java/1-50/ReverseNodesInKGroup.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class ReverseNodesInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode node = head;
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        node = head;
+        ListNode prev = null;
+        ListNode vhead = new ListNode(0);
+        vhead.next = head;
+        ListNode prevEnd = vhead;
+        for (int i = 0; i < count / k; i++) {
+            for (int j = 0; j < k; j++) {
+                ListNode next = node.next;
+                node.next = prev;
+                prev = node;
+                node = next;
+            }
+            ListNode tail = prevEnd.next;
+            prevEnd.next = prev;
+            tail.next = node;
+            prevEnd = tail;
+            prev = null;
+        }
+        return vhead.next;
+    }
+}
