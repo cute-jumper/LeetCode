11c57a6c958212daa5ae84b936665cd95d96e8e5
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-14 18:19:40 -0400

    Add insert delete get random duplicates allowed

---
+++ Java/351-400/InsertDeleteGetRandom2.java
@@ -0,0 +1,58 @@
+public class RandomizedCollection {
+
+    Map<Integer, Set<Integer>> map;
+    List<Integer> store;
+    Random rand;
+
+    /** Initialize your data structure here. */
+    public RandomizedCollection() {
+        map = new HashMap<>();
+        store =  new ArrayList<>();
+        rand = new Random();
+    }
+
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        boolean contains = map.containsKey(val);
+        store.add(val);
+        if (!contains) {
+            map.put(val, new HashSet<>());
+        }
+        map.get(val).add(store.size() - 1);
+        return !contains;
+    }
+
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        Set<Integer> indices = map.get(val);
+        Iterator<Integer> iter = indices.iterator();
+        int index = iter.next();
+        iter.remove();
+        if (indices.size() == 0) {
+            map.remove(val);
+        }
+        if (index == store.size() - 1) store.remove(index);
+        else {
+            int swap = store.remove(store.size() - 1);
+            Set<Integer> set = map.get(swap);
+            set.remove(store.size());
+            set.add(index);
+            store.set(index, swap);
+        }
+        return true;
+    }
+
+    /** Get a random element from the collection. */
+    public int getRandom() {
+        return store.get(rand.nextInt(store.size()));
+    }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
