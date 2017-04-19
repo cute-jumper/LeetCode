eebee6702ab7c341ffd8115df98ffb832f26e85a
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-19 13:10:17 -0400

    Simplify student attendance record 2

--- Java/551-600/StudentAttendanceRecord2.java
+++ Java/551-600/StudentAttendanceRecord2.java
@@ -1,22 +1,19 @@
 public class StudentAttendanceRecord2 {
-    public int checkRecord(int n) {
-        if (n == 1) return 3;
-        long mod = 1000000000l + 7;
-        long[][] dp = new long[n][3];
-        dp[0][0] = 1;
-        dp[0][1] = 1;
-        dp[0][2] = 0;
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % mod;
-            dp[i][1] = dp[i-1][0];
-            dp[i][2] = dp[i-1][1];
+    public class Solution {
+        public int checkRecord(int n) {
+            if (n == 1) return 3;
+            long mod = 1000000000l + 7;
+            long[] dp = new long[n];
+            dp[0] = 2;
+            for (int i = 1; i < n; i++) {
+                dp[i] = (dp[i-1] + (i > 1 ? dp[i-2] : 1) + (i > 2 ? dp[i-3] : 1)) % mod;
+            }
+            long res = dp[n-1];
+            for (int i = 0; i < n; i++) {
+                res += ((i > 0 ? dp[i-1] : 1) * (i < n -1 ? dp[n-2-i] : 1)) % mod;
+                res %= mod;
+            }
+            return (int)res;
         }
-        long res = dp[n-1][0] + dp[n-1][1] + dp[n-1][2];
-        res %= mod;
-        for (int i = 0; i < n; i++) {
-            res += (i > 0 ? (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % mod : 1) * (i < n - 1 ? (dp[n-2-i][0] + dp[n-2-i][1] + dp[n-2-i][2]) % mod : 1);
-            res %= mod;
-        }
-        return (int) (res % mod);
     }
 }
