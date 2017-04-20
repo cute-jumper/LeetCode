5abfb07bed487e0f8789dd195ac370e9751d9d6a
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-20 14:14:15 -0400

    Update reverse integer

--- Java/1-50/ReverseInteger.java
+++ Java/1-50/ReverseInteger.java
@@ -1,24 +1,11 @@
 public class ReverseInteger {
     public int reverse(int x) {
         int res = 0;
-        int value = Math.abs(x);
-        final int MAX_VALUE = Integer.MAX_VALUE / 10;
-        while (value > 0) {
-            if (res < 0 || res > MAX_VALUE) {
-                return 0;
-            }
-            int digit = value % 10;
-            value /= 10;
-            res *= 10;
-            res += digit;
-            if (res < 0) {
-                if (x > 0 || res > Integer.MIN_VALUE) {
-                    return 0;
-                }
-            }
-        }
-        if (x < 0) {
-            return -res;
+        while (x != 0) {
+            int next = res * 10 + x % 10;
+            if (next / 10 != res) return 0;
+            x /= 10;
+            res = next;
         }
         return res;
     }
