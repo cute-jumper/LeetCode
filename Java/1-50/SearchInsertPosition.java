a03411dc83a24ff78cb0ec5b935e6660a099208f
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-20 18:12:54 -0400

    Update search insert position

--- Java/1-50/SearchInsertPosition.java
+++ Java/1-50/SearchInsertPosition.java
@@ -1,23 +1,13 @@
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        if (nums.length == 0) return 0;
-        int high = nums.length - 1;
         int low = 0;
+        int high = nums.length - 1;
         while (low < high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) low = mid + 1;
+            else high = mid;
         }
-        if (nums[low] > target) {
-            return low;
-        } else if (nums[high] < target) {
-            return high + 1;
-        }
-        return high;
+        return nums[low] < target ? nums.length : low;
     }
 }
