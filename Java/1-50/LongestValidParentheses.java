5cf3989c3371e643cbf7d3f6518dbff7a1cc0f34
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-20 17:39:42 -0400

    Update longest valid parentheses

--- Java/1-50/LongestValidParentheses.java
+++ Java/1-50/LongestValidParentheses.java
@@ -1,34 +1,16 @@
 public class LongestValidParentheses {
     public int longestValidParentheses(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        int maxLen = 0;
-        int currLen = 0;
-        Map<Integer, Integer> lens = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '(') {
-                if (lens.containsKey(stack.size())) {
-                    lens.put(stack.size(), lens.get(stack.size()) + currLen);
-                } else {
-                    lens.put(stack.size(), currLen);
-                }
-                currLen = 0;
-                maxLen = Math.max(maxLen, lens.get(stack.size()));
-                stack.push(c);
-            } else {
-                if (stack.isEmpty() || stack.peek() != '(') {
-                    lens.clear();
-                    maxLen = Math.max(maxLen, currLen);
-                    currLen = 0;
-                } else {
-                    stack.pop();
-                    currLen += 2;
-                    if (lens.containsKey(stack.size())) {
-                        currLen += lens.remove(stack.size());
-                    }
-                }
-            }
+            if (c == '(' || stack.isEmpty() || s.charAt(stack.peek()) != '(') stack.push(i);
+            else stack.pop();
         }
-        return Math.max(maxLen, currLen);
+        int prev = s.length(), maxLen = 0;
+        while (!stack.isEmpty()) {
+            maxLen = Math.max(maxLen, prev - stack.peek() - 1);
+            prev = stack.pop();
+        }
+        return Math.max(maxLen, prev);
     }
 }
