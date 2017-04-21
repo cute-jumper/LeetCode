7e20d75af6d98f20cde40733482aa29287e70368
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-21 15:51:22 -0400

    Update plus one

--- Java/51-100/PlusOne.java
+++ Java/51-100/PlusOne.java
@@ -1,21 +1,14 @@
 public class PlusOne {
     public int[] plusOne(int[] digits) {
-        int carry = 1;
-        int[] res = new int[digits.length];
         for (int i = digits.length - 1; i >= 0; i--) {
-            int s = digits[i] + carry;
-            carry = s / 10;
-            res[i] = s % 10;
-        }
-        if (carry == 0) {
-            return res;
-        } else {
-            int[] newRes = new int[res.length + 1];
-            newRes[0] = 1;
-            for (int i = 0; i < res.length; i++) {
-                newRes[i + 1] = res[i];
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            return newRes;
+            digits[i] = 0;
         }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
     }
 }
