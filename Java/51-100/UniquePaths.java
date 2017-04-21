8464265f9f05f49e7943ae932aa75687b78f7fc6
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-21 15:40:53 -0400

    Update unique paths

--- Java/51-100/UniquePaths.java
+++ Java/51-100/UniquePaths.java
@@ -1,21 +1,12 @@
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-        m--;
-        n--;
-        if (m == 0 || n == 0) return 1;
-        long denom = 1;
-        long nomen = 1;
-        int minValue = Math.min(m, n);
-        int start = m + n - minValue + 1;
-        for (int i = 0; i < minValue; i++) {
-            denom *= start;
-            nomen *= minValue - i;
-            if (denom % nomen == 0) {
-                denom /= nomen;
-                nomen = 1;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) dp[i] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j-1];
             }
-            start++;
         }
-        return (int)(denom / nomen);
+        return dp[n-1];
     }
 }
