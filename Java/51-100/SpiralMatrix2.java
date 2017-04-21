843ce05721f1b74fae56550de86fd4193c1f144a
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-21 15:23:53 -0400

    Update spiral matrix 2

--- Java/51-100/SpiralMatrix2.java
+++ Java/51-100/SpiralMatrix2.java
@@ -3,22 +3,10 @@ public class SpiralMatrix2 {
         int[][] matrix = new int[n][n];
         int num = 1;
         for (int i = 0; n - 2 * i > 0; i++) {
-            for (int c = i; c <= n - i - 1; c++) {
-                matrix[i][c] = num;
-                num++;
-            }
-            for (int r = i + 1; r < n - 1 - i; r++) {
-                matrix[r][n - i - 1] = num;
-                num++;
-            }
-            for (int c = n - 1 - i; c > i; c--) {
-                matrix[n - 1 - i][c] = num;
-                num++;
-            }
-            for (int r = n - 1 - i; r > i; r--) {
-                matrix[r][i] = num;
-                num++;
-            }
+            for (int c = i; c <= n - i - 1; c++) matrix[i][c] = num++;
+            for (int r = i + 1; r < n - 1 - i; r++) matrix[r][n - i - 1] = num++;
+            for (int c = n - 1 - i; c > i; c--) matrix[n - 1 - i][c] = num++;
+            for (int r = n - 1 - i; r > i; r--) matrix[r][i] = num;
         }
         return matrix;
     }
