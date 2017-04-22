48d21e5cbdebf0d7861d0042f391d38fa25d2761
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-22 18:05:59 -0400

    Update find peak element

--- Java/151-200/FindPeakElement.java
+++ Java/151-200/FindPeakElement.java
@@ -1,10 +1,11 @@
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < nums[i - 1]) {
-                return i - 1;
-            }
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) high = mid;
+            else if (nums[mid] < nums[mid + 1]) low = mid + 1;
         }
-        return nums.length - 1;
+        return low;
     }
 }
