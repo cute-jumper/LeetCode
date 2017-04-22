1f8df1558386bc3aac1839186146e209f6cda623
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-22 15:21:18 -0400

    Add O(n) space method for candy

--- Java/101-150/Candy.java
+++ Java/101-150/Candy.java
@@ -28,5 +28,23 @@ public class Candy {
             prev = ratings[i];
         }
         return total;
+        // if (ratings.length == 0) return 0;
+        // int[] candies = new int[ratings.length];
+        // int current = 1;
+        // for (int i = 0; i < ratings.length; i++) {
+        //     if (i == 0 || ratings[i] <= ratings[i-1]) {
+        //         candies[i] = current = 1;
+        //     } else {
+        //         candies[i] = ++current;
+        //     }
+        // }
+        // int total = candies[ratings.length - 1];
+        // for (int i = ratings.length - 1; i >= 1; i--) {
+        //     if (ratings[i-1] > ratings[i]) {
+        //         candies[i-1] = Math.max(candies[i-1], candies[i] + 1);
+        //     }
+        //     total += candies[i-1];
+        // }
+        // return total;
     }
 }
