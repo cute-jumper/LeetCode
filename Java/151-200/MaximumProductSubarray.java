c9a7292a3ae4328d3a154d51db4b4750965cbab2
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-22 17:34:35 -0400

    Update maximum product subarray

--- Java/151-200/MaximumProductSubarray.java
+++ Java/151-200/MaximumProductSubarray.java
@@ -1,20 +1,17 @@
 public class MaximumProductSubarray {
     public int maxProduct(int[] nums) {
-        int[] minprod = new int[nums.length + 1];
-        int[] maxprod = new int[nums.length + 1];
-        minprod[0] = maxprod[0] = 1;
+        int minprod = 1, maxprod = 1;
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0) {
-                minprod[i + 1] = Math.min(maxprod[i] * nums[i], nums[i]);
-                maxprod[i + 1] = Math.max(minprod[i] * nums[i], nums[i]);
+                int oldMin = minprod;
+                minprod = Math.min(maxprod * nums[i], nums[i]);
+                maxprod = Math.max(oldMin * nums[i], nums[i]);
             } else {
-                minprod[i + 1] = Math.min(minprod[i] * nums[i], nums[i]);
-                maxprod[i + 1] = Math.max(maxprod[i] * nums[i], nums[i]);
+                minprod = Math.min(minprod * nums[i], nums[i]);
+                maxprod = Math.max(maxprod * nums[i], nums[i]);
             }
-            if (maxprod[i + 1] > max) {
-                max = maxprod[i + 1];
-            }
+            max = Math.max(max, maxprod);
         }
         return max;
     }
