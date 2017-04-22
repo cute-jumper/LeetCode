0d4d6458ea6ede3c8aaf2114fdf27fdd039b750a
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-22 00:22:15 -0400

    Update balanced binary tree

--- Java/101-150/BalancedBinaryTree.java
+++ Java/101-150/BalancedBinaryTree.java
@@ -10,11 +10,14 @@
 public class BalancedBinaryTree {
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
-        return Math.abs(depth(root.left) - depth(root.right)) < 2 &&
-            isBalanced(root.left) && isBalanced(root.right);
+        return dfs(root) != -1;
     }
-    public int depth(TreeNode root) {
+    public int dfs(TreeNode root) {
         if (root == null) return 0;
-        return 1 + Math.max(depth(root.left), depth(root.right));
+        int left = dfs(root.left);
+        if (left == -1) return -1;
+        int right = dfs(root.right);
+        if (right == -1 || Math.abs(left - right) > 1) return -1;
+        return 1 + Math.max(left, right);
     }
 }
