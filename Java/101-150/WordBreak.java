9577b034ff9a06f8e2b3ccff90eab11c874f8b21
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-22 15:44:14 -0400

    Add DFS+memoization for word break

--- Java/101-150/WordBreak.java
+++ Java/101-150/WordBreak.java
@@ -1,4 +1,21 @@
 public class WordBreak {
+    // public boolean wordBreak(String s, List<String> wordDict) {
+    //     Set<String> wordSet = new HashSet<>();
+    //     for (String word : wordDict) wordSet.add(word);
+    //     return checkWordBreak(new int[s.length()], s, 0, wordSet);
+    // }
+    // public boolean checkWordBreak(int[] memo, String s, int start, Set<String> wordSet) {
+    //     if (start == s.length()) return true;
+    //     if (memo[start] != 0) return memo[start] == 1;
+    //     for (int i = s.length(); i > start; i--) {
+    //         String sub = s.substring(start, i);
+    //         if (wordSet.contains(sub)) {
+    //             if (checkWordBreak(memo, s, i, wordSet))
+    //                 return (memo[start] = 1) == 1;
+    //         }
+    //     }
+    //     return (memo[start] = -1) == 1;
+    // }
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
