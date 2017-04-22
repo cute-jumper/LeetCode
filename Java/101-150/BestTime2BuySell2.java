31bbb9af2e0f892f59bd4aae12a86df7d3ca1bfe
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-22 01:50:16 -0400

    Update best time to buy and sell stocks 2

--- Java/101-150/BestTime2BuySell2.java
+++ Java/101-150/BestTime2BuySell2.java
@@ -1,18 +1,12 @@
 public class BestTime2BuySell2 {
     public int maxProfit(int[] prices) {
         if (prices.length == 0) return 0;
-        int minValue = prices[0];
-        int prev = prices[0];
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < prev) {
-                if (prev > minValue) {
-                    profit += prev - minValue;
-                }
-                minValue = prices[i];
+            if (prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
             }
-            prev = prices[i];
         }
-        return profit + prev - minValue;
+        return profit;
     }
 }
