649748aa7bde6b36481b5c8a6f5d5bede177ed98
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-22 17:30:04 -0400

    Update reverse words in string

--- Java/151-200/ReverseWordsInString.java
+++ Java/151-200/ReverseWordsInString.java
@@ -1,22 +1,19 @@
 public class ReverseWordsInString {
     public String reverseWords(String s) {
         int index = s.length() - 1;
-        int start = -1;
         int end = -1;
         StringBuilder sb = new StringBuilder();
         while (index >= -1) {
             if (index == -1 || s.charAt(index) == ' ') {
-                if (start <= end && start >= 0) {
-                    for (int i = start; i <= end; i++) {
+                if (index < end) {
+                    for (int i = index + 1; i <= end; i++) {
                         sb.append(s.charAt(i));
                     }
                     sb.append(' ');
-                    start = -1;
                     end = -1;
                 }
-            } else {
-                if (end == -1) end = index;
-                start = index;
+            } else if (end == -1) {
+                end = index;
             }
             index--;
         }
