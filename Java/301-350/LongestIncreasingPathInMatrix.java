3277a3be3a7ae26ca657e01a763d8dd4debaff31
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-24 18:32:54 -0400

    Update longest increasing path in matrix

--- Java/301-350/LongestIncreasingPathInMatrix.java
+++ Java/301-350/LongestIncreasingPathInMatrix.java
@@ -5,23 +5,23 @@ public class LongestIncreasingPathInMatrix {
         int[][] memo = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                longest = Math.max(longest, dfs(memo, matrix, i, j, 0));
+                longest = Math.max(longest, dfs(memo, matrix, i, j));
             }
         }
         return longest;
     }
-    public int dfs(int[][] memo, int[][] matrix, int r, int c, int len) {
-        if (memo[r][c] != 0) return memo[r][c] + len;
+    public int dfs(int[][] memo, int[][] matrix, int r, int c) {
+        if (memo[r][c] != 0) return memo[r][c];
         int[][] dirs = new int[][] {{0,-1},{1,0},{0,1},{-1,0}};
-        int maxLen = len + 1;
+        int maxLen = 1;
         for (int[] dir : dirs) {
             int row = r + dir[0];
             int col = c + dir[1];
             if (row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length && matrix[row][col] > matrix[r][c]) {
-                maxLen = Math.max(maxLen, dfs(memo, matrix, row, col, len + 1));
+                maxLen = Math.max(maxLen, 1 + dfs(memo, matrix, row, col));
             }
         }
-        memo[r][c] = maxLen - len;
+        memo[r][c] = maxLen;
         return maxLen;
     }
 }
