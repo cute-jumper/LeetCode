9d7ff08114205616ad754215c3c6449814ad8f7a
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-24 17:53:18 -0400

    Update buy and sell stocks with cool down

--- Java/301-350/BestBuySellStocksCooldown.java
+++ Java/301-350/BestBuySellStocksCooldown.java
@@ -1,11 +1,11 @@
 public class BestBuySellStocksCooldown {
     public int maxProfit(int[] prices) {
-        int s = 0, b = Integer.MIN_VALUE, prev = 0, oldSell = 0;
+        int s = 0, b = Integer.MIN_VALUE, prev2 = 0, prev1 = 0;
         for (int price : prices) {
-            prev = oldSell;
-            oldSell = s;
+            prev2 = prev1;
+            prev1 = s;
             s = Math.max(s, b + price);
-            b = Math.max(prev - price, b);
+            b = Math.max(prev2 - price, b);
         }
         return s;
     }
