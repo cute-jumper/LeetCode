faebcbaea3ceb041e696e2e418a1b50a7c7b78ab
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-25 18:05:23 -0400

    Update Repeated substring pattern

--- Java/451-500/RepeatedSubstringPattern.java
+++ Java/451-500/RepeatedSubstringPattern.java
@@ -1,22 +1,22 @@
 public class RepeatedSubstringPattern {
     public boolean repeatedSubstringPattern(String s) {
-        boolean[] nonPrimes = new boolean[s.length() + 1];
-        for (int i = 2; i < nonPrimes.length; i++) {
-            if (nonPrimes[i]) continue;
-            int index = i + i;
-            while (index < nonPrimes.length) {
-                nonPrimes[index] = true;
-                index += i;
-            }
+        int[] count = new int[256];
+        int min = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
         }
-        int len = 0;
-        for (int i = 2; i < nonPrimes.length; i++) {
-            if (nonPrimes[i] || s.length() % i != 0) continue;
+        for (int i : count) {
+            if (i != 0) min = gcd(i, min);
+        }
+        if (min == 1 || s.length() % min != 0) return false;
+        for (int k = min; k > 1; k--) {
+            if (s.length() % k != 0 || gcd(k, min) == 1) continue;
             boolean found = true;
+            int len = s.length() / k;
             out:
-            for (int j = s.length() / i; j < s.length(); j += s.length() / i) {
-                for (int k = 0; k < s.length() / i; k++) {
-                    if (s.charAt(k) != s.charAt(j + k)) {
+            for (int i = 0; i < len; i++) {
+                for (int j = 1; j < k; j++) {
+                    if (s.charAt(i) != s.charAt(i + j * len)) {
                         found = false;
                         break out;
                     }
@@ -25,5 +25,13 @@ public class RepeatedSubstringPattern {
             if (found) return true;
         }
         return false;
+    }
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        return a;
     }
 }
