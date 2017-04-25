292a22755b0ba1e3498c24d651994c80a759cecf
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-25 17:33:18 -0400

    Update delete node in BST

--- Java/401-450/DeleteNodeInBST.java
+++ Java/401-450/DeleteNodeInBST.java
@@ -9,37 +9,18 @@
  */
 public class DeleteNodeInBST {
     public TreeNode deleteNode(TreeNode root, int key) {
-        TreeNode node = root;
-        TreeNode parent = null;
-        while (node != null && node.val != key) {
-            parent = node;
-            if (node.val < key) node = node.right;
-            else node = node.left;
-        }
-        if (parent == null) return deleteRoot(root);
-        if (parent.left != null && parent.left.val == node.val) {
-            parent.left = deleteRoot(node);
-        } else {
-            parent.right = deleteRoot(node);
-        }
-        return root;
-    }
-    public TreeNode deleteRoot(TreeNode root) {
         if (root == null) return null;
-        if (root.left == null) return root.right;
-        if (root.right == null) return root.left;
-        TreeNode node = root.left;
-        TreeNode parent = root;
-        if (node.right == null) {
-            root.val = node.val;
-            parent.left = deleteRoot(node);
-        } else {
+        if (root.val > key) root.left = deleteNode(root.left, key);
+        else if (root.val < key) root.right = deleteNode(root.right, key);
+        else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode node = root.left;
             while (node.right != null) {
-                parent = node;
                 node = node.right;
             }
             root.val = node.val;
-            parent.right = deleteRoot(node);
+            root.left = deleteNode(root.left, root.val);
         }
         return root;
     }
