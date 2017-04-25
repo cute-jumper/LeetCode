8500944006ee8b9f9a9a59f424b8b1b24f654ce6
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-25 14:41:12 -0400

    Update arithmetic slices

--- Java/401-450/ArithmeticSlices.java
+++ Java/401-450/ArithmeticSlices.java
@@ -1,19 +1,12 @@
 public class ArithmeticSlices {
     public int numberOfArithmeticSlices(int[] A) {
-        int low = 0;
+        int curr = 0;
         int total = 0;
-        while (low < A.length - 2) {
-            if (A[low + 1] - A[low] == A[low + 2] - A[low + 1]) {
-                int diff = A[low + 1] - A[low];
-                for (int i = low + 3; i <= A.length; i++) {
-                    if (i == A.length || A[i] - A[i - 1] != diff) {
-                        total += (i - low - 1) * (i - low - 2) / 2;
-                        low = i - 1;
-                        break;
-                    }
-                }
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i-1] == A[i-1] - A[i-2]) {
+                total += ++curr;
             } else {
-                low++;
+                curr = 0;
             }
         }
         return total;
