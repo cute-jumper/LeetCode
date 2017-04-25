c32387c0f0fea23de04b01572fabf4792d7cd779
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-25 03:23:52 -0400

    Update Nth digit

--- Java/351-400/NthDigit.java
+++ Java/351-400/NthDigit.java
@@ -1,27 +1,13 @@
 public class NthDigit {
     public int findNthDigit(int n) {
-        int acc = 0;
-        int width = 1;
-        int count = 9;
+        int acc = 0, width = 1, start = 1, count = 9;
         while ((n - acc) / width > count) {
             acc += count * width;
             count *= 10;
+            start *= 10;
             width++;
         }
-        int start = 1;
-        for (int i = 1; i < width; i++) {
-            start *= 10;
-        }
-        int end = (n - acc) / width - 1 + start;
-        int k = (n - acc) % width;
-        if (k == 0) return end % 10;
-        end++;
-        int digit = 0;
-        for (int i = 0; i < k; i++) {
-            digit = end / start;
-            end -= start * digit;
-            start /= 10;
-        }
-        return digit;
+        start += (n - acc - 1) / width;
+        return (start + "").charAt((n - acc - 1) % width) - '0';
     }
 }
