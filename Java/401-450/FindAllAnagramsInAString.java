6a4ffd94747b231bf77457ff400e08c057d80767
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-25 16:50:56 -0400

    Update find anagrams in a string

--- Java/401-450/FindAllAnagramsInAString.java
+++ Java/401-450/FindAllAnagramsInAString.java
@@ -1,35 +1,15 @@
 public class FindAllAnagramsInAString {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        if (s.length() < p.length()) return res;
         int[] count = new int[26];
         for (int i = 0; i < p.length(); i++) {
             count[p.charAt(i) - 'a']++;
         }
-        for (int i = 0; i < p.length(); i++) {
-            count[s.charAt(i) - 'a']--;
-        }
-        int nonMatch = 0;
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] != 0) nonMatch++;
-        }
-        if (nonMatch == 0) res.add(0);
-        for (int i = p.length(); i < s.length(); i++) {
-            int c1 = s.charAt(i - p.length()) - 'a';
-            count[c1]++;
-            if (count[c1] == 0) {
-                nonMatch--;
-            } else if (count[c1] == 1){
-                nonMatch++;
-            }
-            int c2 = s.charAt(i) - 'a';
-            count[c2]--;
-            if (count[c2] == 0) {
-                nonMatch--;
-            } else if (count[c2] == -1) {
-                nonMatch++;
-            }
-            if (nonMatch == 0) res.add(i - p.length() + 1);
+        int total = p.length(), left = 0, right = 0;
+        List<Integer> res = new ArrayList<>();
+        while (right < s.length()) {
+            if (count[s.charAt(right++) - 'a']-- >= 1) total--;
+            if (total == 0) res.add(left);
+            if (right - left == p.length() && count[s.charAt(left++) - 'a']++ >= 0) total++;
         }
         return res;
     }
