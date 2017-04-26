12289b63a8f9efe3bd332c0531affb2452b66d32
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-26 13:44:07 -0400

    Update number complement

--- Java/451-500/NumberComplement.java
+++ Java/451-500/NumberComplement.java
@@ -1,12 +1,7 @@
 public class NumberComplement {
     public int findComplement(int num) {
-        int mask = 0;
-        int n = num;
-        for (int i = 31; i >= 0; i--) {
-            if ((n & (1 << 31)) == 0) mask |= 1 << i;
-            else break;
-            n <<= 1;
-        }
+        int mask = -1;
+        while ((num & mask) != 0) mask <<= 1;
         return ~num & ~mask;
     }
 }
