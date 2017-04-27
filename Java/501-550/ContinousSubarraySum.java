e37ae6a76f619aceb916b85cb9ed56c9d3c85edd
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-27 16:43:59 -0400

    Update continuous subarray sum

--- Java/501-550/ContinousSubarraySum.java
+++ Java/501-550/ContinousSubarraySum.java
@@ -1,16 +1,17 @@
 public class ContinouseSubarraySum {
     public boolean checkSubarraySum(int[] nums, int k) {
-        int[] sum = new int[nums.length - 1];
-        for (int i = 0; i < nums.length - 1; i++) {
-            sum[i] = nums[i] + nums[i + 1];
-            if (k == 0 && sum[i] == 0 ||
-                k != 0 && sum[i] % k == 0) return true;
-        }
-        for (int len = 3; len <= nums.length; len++) {
-            for (int i = 0; i + len <= nums.length; i++) {
-                sum[i] += nums[i + len - 1];
-                if (k == 0 && sum[i] == 0 ||
-                    k != 0 && sum[i] % k == 0) return true;
+        k = Math.abs(k);
+        int[] sum = new int[nums.length + 1];
+        Map<Integer, Integer> map = new HashMap<>();
+        sum[0] = 0;
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+            int mod = k != 0 ? sum[i + 1] % k : sum[i + 1];
+            if (map.containsKey(mod)) {
+                if (i - map.get(mod) > 1) return true;
+            } else {
+                map.put(mod, i);
             }
         }
         return false;
