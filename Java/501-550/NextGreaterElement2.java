7af1c90609031ae14cbf1206a07b8fa7f47aeec1
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-27 02:14:13 -0400

    Update next greater element 2

--- Java/501-550/NextGreaterElement2.java
+++ Java/501-550/NextGreaterElement2.java
@@ -1,23 +1,14 @@
 public class NextGreaterElement2 {
     public int[] nextGreaterElements(int[] nums) {
-        Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         int[] res = new int[nums.length];
+        Arrays.fill(res, -1);
         for (int i = 0; i < 2 * nums.length; i++) {
-            if (i >= nums.length) {
-                if (dq.isEmpty()) break;
-                int index = i - nums.length;
-                if (index == dq.peekFirst()) {
-                    res[dq.pollFirst()] = -1;
-                }
-                while (!dq.isEmpty() && nums[index] > nums[dq.peekLast()]) {
-                    res[dq.pollLast()] = nums[index];
-                }
-            } else {
-                while (!dq.isEmpty() && nums[i] > nums[dq.peekLast()]) {
-                    res[dq.pollLast()] = nums[i];
-                }
-                dq.offerLast(i);
+            int n = nums[i % nums.length];
+            while (!stack.isEmpty() && n > nums[stack.peek()]) {
+                res[stack.pop()] = n;
             }
+            if (i < nums.length) stack.push(i);
         }
         return res;
     }
