9fb1f57a268d7ea03a1797ad149e9c52726889d5
Junpeng Qiu <qjpchmail@gmail.com>
2017-04-27 18:04:04 -0400

    Update 01 matrix

--- Java/501-550/ZeroOneMatrix.java
+++ Java/501-550/ZeroOneMatrix.java
@@ -1,34 +1,28 @@
 public class ZeroOneMatrix {
-    public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
-        List<int[]> current = new ArrayList<>();
-        boolean[][] visited = new boolean[matrix.size()][matrix.get(0).size()];
-        for (int i = 0; i < matrix.size(); i++) {
-            List<Integer> row = matrix.get(i);
-            for (int j = 0; j < row.size(); j++) {
-                if (row.get(j) == 0) {
-                    current.add(new int[] { j, i });
-                    visited[i][j] = true;
+    public int[][] updateMatrix(int[][] matrix) {
+        Deque<int[]> current = new ArrayDeque<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    current.offerLast(new int[] { j, i });
+                } else {
+                    matrix[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
-        int dist = 1;
         int[][] dirs = new int[][] {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         while (!current.isEmpty()) {
-            List<int[]> next = new ArrayList<>();
-            for (int[] p : current) {
-                for (int[] dir : dirs) {
-                    int x = p[0] + dir[0];
-                    int y = p[1] + dir[1];
-                    if (y >= 0 && y < matrix.size() && x >= 0 && x < matrix.get(y).size() &&
-                        !visited[y][x] && matrix.get(y).get(x) == 1) {
-                        next.add(new int[] { x, y });
-                        visited[y][x] = true;
-                        if (dist > 1) matrix.get(y).set(x, dist);
-                    }
+            int[] p = current.pollFirst();
+            int dist = matrix[p[1]][p[0]] + 1;
+            for (int[] dir : dirs) {
+                int x = p[0] + dir[0];
+                int y = p[1] + dir[1];
+                if (y >= 0 && y < matrix.length && x >= 0 && x < matrix[y].length &&
+                    matrix[y][x] > dist) {
+                    current.offerLast(new int[] { x, y });
+                    matrix[y][x] = dist;
                 }
             }
-            current = next;
-            dist++;
         }
         return matrix;
     }
