c6dfadbee0bbb65def68d09cdef89df1e8a4f3c4
Junpeng Qiu <qjpchmail@gmail.com>
2017-05-04 16:41:20 -0400

    Add maximum size subarray sum equals k

---
+++ Java/301-350/MaxSizeSubarraySumK.java
@@ -0,0 +1,16 @@
+public class MaxSizeSubarraySumK {
+    public int maxSubArrayLen(int[] nums, int k) {
+        int sum = 0;
+        int maxLen = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                maxLen = Math.max(maxLen, i - map.get(sum - k));
+            }
+            if (!map.containsKey(sum)) map.put(sum, i);
+        }
+        return maxLen;
+    }
+}
