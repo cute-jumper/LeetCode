f038eea5528b5d47340e65555a6fc588510324fb
Junpeng Qiu <qjpchmail@gmail.com>
2017-05-05 16:08:58 -0400

    Add reverse words in a string 2

---
+++ Java/151-200/ReverseWordsInAString2.java
@@ -0,0 +1,21 @@
+public class ReverseWordsInAString2 {
+    public void reverseWords(char[] s) {
+        reverse(s, 0, s.length - 1);
+        int start = 0;
+        for (int i = 0; i <= s.length; i++) {
+            if (i == s.length || s[i] == ' ') {
+                reverse(s, start, i - 1);
+                start = i + 1;
+            }
+        }
+    }
+    public void reverse(char[] s, int start, int end) {
+        while (start < end) {
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+}
