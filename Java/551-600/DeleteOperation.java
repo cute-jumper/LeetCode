82b06a535415d8511adde9b7a7db75df1ce022f1
Junpeng Qiu <qjpchmail@gmail.com>
2017-05-21 20:30:12 -0400

    Add delete operation for two strings

---
+++ Java/551-600/DeleteOperation.java
@@ -0,0 +1,15 @@
+public class DeleteOperation {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < word1.length(); i++) {
+            for (int j = 0; j < word2.length(); j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                } else {
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return word1.length() + word2.length() - 2 * dp[word1.length()][word2.length()];
+    }
+}
