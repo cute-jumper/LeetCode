0ffe3515b5eebad83a7c7d8dc2a08e3d7eafa6f3
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 14:46:28 -0700

    Simplify letter combinations using dfs and bfs

--- Java/1-50/LetterCombinations.java
+++ Java/1-50/LetterCombinations.java
@@ -1,27 +1,35 @@
 public class LetterCombinations {
-    public static String[] map = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
-        int[] pos = new int[digits.length()];
-        List<String> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        int index = pos.length - 1;
-        while (index != -1) {
-            sb.setLength(0);
-            for (int i = 0; i < pos.length; i++) {
-                String s = map[digits.charAt(i) - '0'];
-                if (s.length() == 0) return res;
-                sb.append(s.charAt(pos[i]));
-            }
-            res.add(sb.toString());
-            for (index = pos.length - 1; index >=0; index--) {
-                if (pos[index] == map[digits.charAt(index) - '0'].length() - 1) {
-                    pos[index] = 0;
-                } else {
-                    pos[index]++;
-                    break;
-                }
-            }
-        }
+    	List<String> res = new ArrayList<>();
+        if (digits.length() == 0) return res;
+        dfs(res, digits, "", 0);
         return res;
     }
+    void dfs(List<String> res, String digits, String curr, int start) {
+        if (start == digits.length()) {
+            res.add(curr);
+            return;
+        }
+        String cands = map[digits.charAt(start) - '0'];
+        for (int i = 0; i < cands.length(); i++) {
+            dfs(res, digits, curr + cands.charAt(i), start + 1);
+        }
+    }
+    // public List<String> letterCombinations(String digits) {
+    //     LinkedList<String> queue = new LinkedList<>();
+    //     if (digits.length() == 0) return queue;
+    //     queue.offer("");
+    //     for (int i = 0; i < digits.length(); i++) {
+    //         String cands = map[digits.charAt(i) - '0'];
+    //         while (!queue.isEmpty() && queue.peek().length() == i) {
+    //             String s = queue.poll();
+    //             for (int j = 0; j < cands.length(); j++) {
+    //                 queue.offer(s + cands.charAt(j));
+    //             }
+    //         }
+    //     }
+    //     return queue;
+    // }
+
 }
