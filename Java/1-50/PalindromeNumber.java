aabe2b3f7d423cb86209520d841931c437a3e33f
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 11:59:14 -0700

    Simplify palindrome number

--- Java/1-50/PalindromeNumber.java
+++ Java/1-50/PalindromeNumber.java
@@ -1,27 +1,11 @@
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
-        int divisor = getLargestDivisor(x);
-        while (divisor > 9) {
-            if (x / divisor == x % 10) {
-                x %= divisor;
-                x /= 10;
-                divisor /= 100;
-            } else {
-                return false;
-            }
+        if (x < 0) return false;
+        int div = 1;
+        while (div <= x / 10) div *= 10;
+        for (int i = x; div > 9; i = (i % div) / 10, div /= 100) {
+            if (i % 10 != i / div) return false;
         }
         return true;
-    }
-
-    public int getLargestDivisor(int x) {
-        int divisor = 1;
-        while (x > 9) {
-            x /= 10;
-            divisor *= 10;
-        }
-        return divisor;
     }
 }
