58a8cf866254a643fa3ffcee1c0e623c001fd848
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 12:00:23 -0700

    Add simpler logic for palindrome number

--- Java/1-50/PalindromeNumber.java
+++ Java/1-50/PalindromeNumber.java
@@ -8,4 +8,14 @@ public class PalindromeNumber {
         }
         return true;
     }
+    // Simpler
+    // public boolean isPalindrome(int x) {
+    //     if (x < 0) return false;
+    //     int div = 1;
+    //     while (div <= x / 10) div *= 10;
+    //     for (int i = x; i > 0; i /= 10, div /= 10) {
+    //         if (i % 10 != (x / div) % 10) return false;
+    //     }
+    //     return true;
+    // }
 }
