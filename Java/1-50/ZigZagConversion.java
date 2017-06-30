497161b90710085da6cfbf7836d12a109eb4da28
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 11:20:11 -0700

    Update zigzag conversion

--- Java/1-50/ZigZagConversion.java
+++ Java/1-50/ZigZagConversion.java
@@ -1,24 +1,14 @@
 public class ZigZagConversion {
     public String convert(String s, int numRows) {
-        if (numRows == 1) {
-            return s;
-        }
+        if (numRows == 1) return s;
         StringBuilder sb = new StringBuilder();
-        int groupNum = 2 * numRows - 2;
-        int index = 0;
+        int len = s.length();
+        int groupCount = 2 * numRows - 2;
         for (int i = 0; i < numRows; i++) {
-            index = i;
-            while (index < s.length()) {
-                sb.append(s.charAt(index));
-                if (!(i == 0 || i == numRows - 1)) {
-                    int newIndex = index + groupNum - 2 * (index % groupNum);
-                    if (newIndex < s.length()) {
-                        sb.append(s.charAt(newIndex));
-                    } else {
-                        break;
-                    }
-                }
-                index += groupNum;
+            for (int j = i; j < len; j += groupCount) {
+                sb.append(s.charAt(j));
+                int next = j + groupCount - 2 * i;
+               	if (i > 0 && i < numRows - 1 && next < len) sb.append(s.charAt(next));
             }
         }
         return sb.toString();
