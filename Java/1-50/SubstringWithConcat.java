b7fe3491bb1524bc686e64fc06cef158eafbde53
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 17:09:35 -0700

    Simplify substring with concat of all words

--- Java/1-50/SubstringWithConcat.java
+++ Java/1-50/SubstringWithConcat.java
@@ -2,40 +2,33 @@ public class SubstringWithConcat {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> res = new ArrayList<>();
         if (words.length == 0) return res;
-        int wordLen = words[0].length();
-        int totalLen = wordLen * words.length;
-        if (s.length() < totalLen) return res;
-        Map<String, Integer> dict = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (String w : words) {
-            if (dict.containsKey(w)) dict.put(w, dict.get(w) + 1);
-            else dict.put(w, 1);
-        }
-        for (int start = 0; start < wordLen && start + totalLen <= s.length(); start++) {
-            Map<String, Integer> map = new HashMap<>(dict);
-            int count = 0;
-            for (int i = start; i < start + totalLen; i += wordLen) {
-                String w = s.substring(i, i + wordLen);
-                if (map.containsKey(w)) {
-                    map.put(w, map.get(w) - 1);
-                    if (map.get(w) == 0) count++;
-                    else if (map.get(w) == -1) count--;
-                }
+            if (map.containsKey(w)) {
+                map.put(w, map.get(w) + 1);
+            } else {
+                map.put(w, 1);
             }
-            if (count == dict.size()) res.add(start);
-            for (int i = start + wordLen; i + totalLen <= s.length(); i += wordLen) {
-                String w = s.substring(i - wordLen, i);
-                if (dict.containsKey(w)) {
-                    map.put(w, map.get(w) + 1);
-                    if (map.get(w) == 0) count++;
-                    else if (map.get(w) == 1) count--;
+        }
+        int wl = words[0].length();
+        int totalLen = wl * words.length;
+        for (int i = 0; i < wl; i++) {
+            Map<String, Integer> m = new HashMap<>(map);
+            int count = words.length;
+            for (int j = i, k = 0; j + wl <= s.length(); j += wl, k++) {
+                String str = s.substring(j, j + wl);
+                if (m.containsKey(str)) {
+                    m.put(str, m.get(str) - 1);
+                    if (m.get(str) >= 0) count--;
                 }
-                w = s.substring(i + totalLen - wordLen, i + totalLen);
-                if (map.containsKey(w)) {
-                    map.put(w, map.get(w) - 1);
-                    if (map.get(w) == 0) count++;
-                    else if (map.get(w) == -1) count--;
+                if (k >= words.length) {
+                    str = s.substring(j - totalLen, j - totalLen + wl);
+                    if (m.containsKey(str)) {
+                        m.put(str, m.get(str) + 1);
+                        if (m.get(str) > 0) count++;
+                    }
                 }
-                if (count == dict.size()) res.add(i);
+                if (count == 0) res.add(j - totalLen + wl);
             }
         }
         return res;
