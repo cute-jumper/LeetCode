eeebf2eb2614deaa7480dd069a52f6ccd4cf83ed
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 18:34:53 -0700

    Simplify search in rotated sorted array

--- Java/1-50/SearchInRotatedSortedArray.java
+++ Java/1-50/SearchInRotatedSortedArray.java
@@ -1,25 +1,21 @@
 public class SearchInsertPosition {
     public int search(int[] nums, int target) {
         if (nums.length == 0) return -1;
-        int low = 0;
-        int high = nums.length - 1;
+        int low = 0, high = nums.length - 1, first = nums[0], last = nums[nums.length - 1];
         int type = 0;
-        if (nums[0] > nums[nums.length - 1]) {
-            if (target >= nums[0]) type = 1;
+        if (first > last) {
+            if (target > last) type = 1;
             else type = 2;
         }
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target)
-                return mid;
-            if (type == 1 && nums[mid] < nums[0]) {
-                high = mid - 1;
-            } else if (type == 2 && nums[mid] > nums[nums.length - 1]) {
-                low = mid + 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) {
+                if (type != 2 || nums[mid] <= last) high = mid - 1;
+                else low = mid + 1;
             } else {
-                high = mid - 1;
+                if (type != 1 || nums[mid] >= first) low = mid + 1;
+                else high = mid - 1;
             }
         }
         return -1;
