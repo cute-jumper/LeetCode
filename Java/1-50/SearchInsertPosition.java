d4e0058c4027aa9ac32ca77b21c615136f323811
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 18:47:44 -0700

    Simplify search insert position

--- Java/1-50/SearchInsertPosition.java
+++ Java/1-50/SearchInsertPosition.java
@@ -1,13 +1,12 @@
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
+        int low = 0, high = nums.length;
         while (low < high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] == target) return mid;
             if (nums[mid] < target) low = mid + 1;
             else high = mid;
         }
-        return nums[low] < target ? nums.length : low;
+        return low;
     }
 }
