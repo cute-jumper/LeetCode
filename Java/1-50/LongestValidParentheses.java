407774f036fe56c34fea78f69395323fffc7278f
Junpeng Qiu <qjpchmail@gmail.com>
2017-06-30 18:15:17 -0700

    Simplify longest valid parentheses in one pass

--- Java/1-50/LongestValidParentheses.java
+++ Java/1-50/LongestValidParentheses.java
@@ -1,16 +1,17 @@
 public class LongestValidParentheses {
     public int longestValidParentheses(String s) {
         Deque<Integer> stack = new ArrayDeque<>();
+        int max = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '(' || stack.isEmpty() || s.charAt(stack.peek()) != '(') stack.push(i);
-            else stack.pop();
+            if (c == '(' || stack.isEmpty() || s.charAt(stack.peek()) != '(')
+                stack.push(i);
+            else {
+                stack.pop();
+                if (stack.isEmpty()) max = Math.max(max, i + 1);
+                else max = Math.max(max, i - stack.peek());
+            }
         }
-        int prev = s.length(), maxLen = 0;
-        while (!stack.isEmpty()) {
-            maxLen = Math.max(maxLen, prev - stack.peek() - 1);
-            prev = stack.pop();
-        }
-        return Math.max(maxLen, prev);
+        return max;
     }
 }
