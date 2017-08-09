bb947da5839cc1fe41e49d63b8f2d49d39630c9e
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-09 17:28:23 -0400

    Update add binary

--- Java/51-100/AddBinary.java
+++ Java/51-100/AddBinary.java
@@ -1,29 +1,21 @@
 public class AddBinary {
     public String addBinary(String a, String b) {
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
         StringBuilder sb = new StringBuilder();
-        while (i >= 0 && j >= 0) {
-            int s = (a.charAt(i) - '0') + (b.charAt(j) - '0') + carry;
-            carry = s / 2;
-            sb.append(s % 2);
-            i--;
-            j--;
+        int lenA = a.length(), lenB = b.length();
+        int carry = 0;
+        for (int i = 0; i < lenA || i < lenB; i++) {
+            int c1 = 0, c2 = 0;
+            if (i < lenA) {
+                c1 = a.charAt(lenA - 1 - i) - '0';
+            }
+            if (i < lenB) {
+                c2 = b.charAt(lenB - 1 - i) - '0';
+            }
+            int s = c1 + c2 + carry;
+            sb.append(s & 1);
+            carry = (s >> 1) & 1;
         }
-        while (i >= 0) {
-            int s = a.charAt(i) - '0' + carry;
-            carry = s / 2;
-            sb.append(s % 2);
-            i--;
-        }
-        while (j >= 0) {
-            int s = b.charAt(j) - '0' + carry;
-            carry = s / 2;
-            sb.append(s % 2);
-            j--;
-        }
-        if (carry == 1) sb.append(carry);
+        if (carry > 0) sb.append(carry);
         return sb.reverse().toString();
     }
 }
