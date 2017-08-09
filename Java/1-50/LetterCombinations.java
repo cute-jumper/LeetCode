1c05a9eedb8aee9a95051f2f95be49d3fa68fd60
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-09 18:16:28 -0400

    Add letter combinations

--- Java/1-50/LetterCombinations.java
+++ Java/1-50/LetterCombinations.java
@@ -1,19 +1,18 @@
 public class LetterCombinations {
     String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
-    	List<String> res = new ArrayList<>();
-        if (digits.length() == 0) return res;
-        dfs(res, digits, "", 0);
+        List<String> res = new ArrayList<>();
+        dfs(res, digits, 0, "");
         return res;
     }
-    void dfs(List<String> res, String digits, String curr, int start) {
+    void dfs(List<String> res, String digits, int start, String curr) {
         if (start == digits.length()) {
-            res.add(curr);
+            if (curr.length() > 0) res.add(curr);
             return;
         }
-        String cands = map[digits.charAt(start) - '0'];
-        for (int i = 0; i < cands.length(); i++) {
-            dfs(res, digits, curr + cands.charAt(i), start + 1);
+        String letters = map[digits.charAt(start) - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            dfs(res, digits, start + 1, curr + letters.charAt(i));
         }
     }
     // public List<String> letterCombinations(String digits) {
