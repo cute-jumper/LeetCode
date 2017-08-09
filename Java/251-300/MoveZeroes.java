b608e69fcf6d0a7fcd94b6ac796fd80607b0d54b
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-09 16:39:26 -0400

    Update move zeros

--- Java/251-300/MoveZeroes.java
+++ Java/251-300/MoveZeroes.java
@@ -3,12 +3,14 @@ public class MoveZeroes {
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[index] = nums[i];
+                swap(nums, index, i);
                 index++;
             }
         }
-        for (int i = index; i < nums.length; i++) {
-            nums[i] = 0;
-        }
+    }
+    void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
