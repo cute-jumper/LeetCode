502a37a0110e983d9823b68ab0fdf43dcc66ea8e
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-09 19:22:11 -0400

    Update first bad version

--- Java/251-300/FirstBadVersion.java
+++ Java/251-300/FirstBadVersion.java
@@ -5,16 +5,14 @@ public class FirstBadVersion extends VersionControl {
     public int firstBadVersion(int n) {
         int low = 1;
         int high = n;
-        int firstBad = n;
-        while (low <= high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
             if (isBadVersion(mid)) {
-                firstBad = mid;
-                high = mid - 1;
+                high = mid;
             } else {
                 low = mid + 1;
             }
         }
-        return firstBad;
+        return low;
     }
 }
