bd7191b3151a9c37e623e0e47fbb1883fa01222b
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-09 20:42:49 -0400

    Update meeting rooms 2

--- Java/251-300/MeetingRooms2.java
+++ Java/251-300/MeetingRooms2.java
@@ -11,26 +11,15 @@ public class MeetingRooms2 {
     public int minMeetingRooms(Interval[] intervals) {
         if (intervals.length < 2) return intervals.length;
         Arrays.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval a, Interval b) {
-                return a.start - b.start;
-            }
-        });
-        PriorityQueue<Interval> pq = new PriorityQueue<>(10, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval a, Interval b) {
-                return a.end - b.end;
-            }
-        });
-        pq.add(intervals[0]);
-        for (int i = 1; i < intervals.length; i++) {
-            Interval interval = pq.poll();
-            if (intervals[i].start >= interval.end) {
-                interval.end = intervals[i].end;
-            } else {
-                pq.offer(intervals[i]);
-            }
-            pq.offer(interval);
+                @Override
+                public int compare(Interval a, Interval b) {
+                    return a.start - b.start;
+                }
+            });
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (Interval it : intervals) {
+            if (!pq.isEmpty() && pq.peek() <= it.start) pq.poll();
+            pq.offer(it.end);
         }
         return pq.size();
     }
