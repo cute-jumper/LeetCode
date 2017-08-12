95e5b4b33358487e5deaceabb8e79a9150b95066
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-12 15:11:09 -0400

    Update one edit distance

--- Java/151-200/OneEditDistance.java
+++ Java/151-200/OneEditDistance.java
@@ -1,19 +1,19 @@
 public class OneEditDistance {
     public boolean isOneEditDistance(String s, String t) {
-        if (Math.abs(s.length() - t.length()) > 1) return false;
-        if (s.length() < t.length()) return isOneEditDistance(t, s);
+        if (s.length() > t.length()) return isOneEditDistance(t, s);
+        if (t.length() - s.length() > 1) return false;
         if (s.length() == t.length()) {
             int diff = 0;
             for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) != t.charAt(i)) diff++;
+                if (s.charAt(i) != t.charAt(i)) {
+                    if (diff++ == 1) return false;
+                }
             }
             return diff == 1;
         }
-        int tIndex = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (tIndex < i - 1) return false;
-            if (tIndex < t.length() && s.charAt(i) == t.charAt(tIndex)) tIndex++;
+            if (s.charAt(i) != t.charAt(i)) return s.substring(i).equals(t.substring(i + 1));
         }
-        return tIndex == t.length();
+        return true;
     }
 }
