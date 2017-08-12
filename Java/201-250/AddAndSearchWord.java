420b1158081b875c7bc382b8c8b0f39c188f62ce
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-12 17:50:40 -0400

    Add dfs version of Trie

--- Java/201-250/AddAndSearchWord.java
+++ Java/201-250/AddAndSearchWord.java
@@ -89,3 +89,70 @@ public class WordDictionary {
  * obj.addWord(word);
  * boolean param_2 = obj.search(word);
  */
+// public class WordDictionary {
+//     class Trie {
+//         boolean end = false;
+//         Trie[] children = new Trie[26];
+//         Trie addChild(char c) {
+//             children[c - 'a'] = new Trie();
+//             return children[c - 'a'];
+//         }
+//         boolean isWord() {
+//             return end;
+//         }
+//         boolean hasChild(char c) {
+//             return children[c - 'a'] != null;
+//         }
+//         Trie getChild(char c) {
+//             return children[c - 'a'];
+//         }
+//         void setWord(boolean val) {
+//             end = val;
+//         }
+//     }
+
+//     Trie root;
+//     /** Initialize your data structure here. */
+//     public WordDictionary() {
+//         root = new Trie();
+//     }
+
+//     /** Adds a word into the data structure. */
+//     public void addWord(String word) {
+//         Trie node = root;
+//         for (int i = 0; i < word.length(); i++) {
+//             char c = word.charAt(i);
+//             if (node.hasChild(c)) node = node.getChild(c);
+//             else node = node.addChild(c);
+//         }
+//         node.setWord(true);
+//     }
+
+//     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+//     public boolean search(String word) {
+//         return search(word, 0, root);
+//     }
+//     private boolean search(String word, int start, Trie node) {
+//         for (int i = start; i < word.length(); i++) {
+//             char c = word.charAt(i);
+//             if (c == '.') {
+//                 for (char j = 'a'; j <= 'z'; j++) {
+//                     if (node.hasChild(j) && search(word, i + 1, node.getChild(j))) return true;
+//                 }
+//                 return false;
+//             } else if (node.hasChild(c)) {
+//                 node = node.getChild(c);
+//             } else {
+//                 return false;
+//             }
+//         }
+//         return node.isWord();
+//     }
+// }
+
+// /**
+//  * Your WordDictionary object will be instantiated and called as such:
+//  * WordDictionary obj = new WordDictionary();
+//  * obj.addWord(word);
+//  * boolean param_2 = obj.search(word);
+//  */
