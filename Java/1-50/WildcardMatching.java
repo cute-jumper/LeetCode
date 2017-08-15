f8c07e7810cb18e94f2e1676c75b368d3fe1c174
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-15 14:56:52 -0400

    Add dp for wildcard matching

--- Java/1-50/WildcardMatching.java
+++ Java/1-50/WildcardMatching.java
@@ -1,27 +1,42 @@
 public class WildcardMatching {
     public boolean isMatch(String s, String p) {
-        int sIndex = 0;
-        int pIndex = 0;
-        int aIndex = -1;
-        int mIndex = -1;
-        while (sIndex < s.length()) {
-            if (pIndex < p.length() && (p.charAt(pIndex) == '?' || p.charAt(pIndex) == s.charAt(sIndex))) {
-                pIndex++;
-                sIndex++;
-            } else if (pIndex < p.length() && p.charAt(pIndex) == '*') {
-                aIndex = pIndex;
-                mIndex = sIndex;
-                pIndex++;
-            } else {
-                if (aIndex == -1) return false;
-                pIndex = aIndex + 1;
-                sIndex = ++mIndex;
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= p.length(); i++) {
+            dp[0][i] = dp[0][i - 1] && p.charAt(i - 1) == '*';
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && (p.charAt(j - 1) == '?' || p.charAt(j - 1) == s.charAt(i - 1));
+                }
             }
         }
-        while (pIndex < p.length()) {
-            if (p.charAt(pIndex) != '*') return false;
-            pIndex++;
-        }
-        return true;
+        return dp[s.length()][p.length()];
+        // int sIndex = 0;
+        // int pIndex = 0;
+        // int aIndex = -1;
+        // int mIndex = -1;
+        // while (sIndex < s.length()) {
+        //     if (pIndex < p.length() && (p.charAt(pIndex) == '?' || p.charAt(pIndex) == s.charAt(sIndex))) {
+        //         pIndex++;
+        //         sIndex++;
+        //     } else if (pIndex < p.length() && p.charAt(pIndex) == '*') {
+        //         aIndex = pIndex;
+        //         mIndex = sIndex;
+        //         pIndex++;
+        //     } else {
+        //         if (aIndex == -1) return false;
+        //         pIndex = aIndex + 1;
+        //         sIndex = ++mIndex;
+        //     }
+        // }
+        // while (pIndex < p.length()) {
+        //     if (p.charAt(pIndex) != '*') return false;
+        //     pIndex++;
+        // }
+        // return true;
     }
 }
