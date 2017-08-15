48541e10b919400faf847d5627b168bc5cd799ad
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-15 15:51:18 -0400

    Update merge sorted array

--- Java/51-100/MergeSortedArray.java
+++ Java/51-100/MergeSortedArray.java
@@ -1,28 +1,10 @@
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = n + m - 1; i >= n; i--) nums1[i] = nums1[i - n];
-        int i = n;
-        int j = 0;
-        int index = 0;
-        while (i < m + n && j < n) {
-            if (nums1[i] > nums2[j]) {
-                nums1[index] = nums2[j];
-                j++;
-            } else {
-                nums1[index] = nums1[i];
-                i++;
-            }
-            index++;
-        }
-        while (i < m + n) {
-            nums1[index] = nums1[i];
-            i++;
-            index++;
-        }
-        while (j < n) {
-            nums1[index] = nums2[j];
-            j++;
-            index++;
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) nums1[k] = nums1[i--];
+            else nums1[k] = nums2[j--];
+            k--;
         }
     }
 }
