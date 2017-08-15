2b72d04ceaa01c0abc7f4454087dd812569faba3
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-15 16:29:43 -0400

    Update word ladder

--- Java/101-150/WordLadder.java
+++ Java/101-150/WordLadder.java
@@ -1,34 +1,29 @@
 public class WordLadder {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        List<String> current = new ArrayList<>();
-        int[] dist = new int[wordList.size()];
-        int depth = 2;
-        current.add(beginWord);
-        while (current.size() > 0) {
-            List<String> next = new ArrayList<>();
-            for (String word : current) {
-                for (int i = 0; i < dist.length; i++) {
-                    if (dist[i] == 0) {
-                        String w = wordList.get(i);
-                        if (strDistance(word, w) == 1) {
-                            if (w.equals(endWord)) return depth;
-                            dist[i] = depth;
-                            next.add(wordList.get(i));
-                        }
+        boolean[] visited = new boolean[wordList.size()];
+        Deque<String> queue = new ArrayDeque<>();
+        queue.offer(beginWord);
+        int step = 1;
+        while (!queue.isEmpty()) {
+            for (int i = 0, size = queue.size(); i < size; i++) {
+                String s = queue.poll();
+                if (s.equals(endWord)) return step;
+                for (int j = 0; j < wordList.size(); j++) {
+                    if (!visited[j] && isOneDiff(s, wordList.get(j))) {
+                        queue.offer(wordList.get(j));
+                        visited[j] = true;
                     }
                 }
             }
-            current = next;
-            depth++;
+            step++;
         }
         return 0;
     }
-
-    public int strDistance(String a, String b) {
-        int dist = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) dist++;
+    boolean isOneDiff(String s, String t) {
+        int diff = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != t.charAt(i)) diff++;
         }
-        return dist;
+        return diff == 1;
     }
 }
