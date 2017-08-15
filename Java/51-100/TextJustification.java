e1ac586145ae643b413be2b76deec021b1c28a4f
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-15 18:59:29 -0400

    Update text justification

--- Java/51-100/TextJustification.java
+++ Java/51-100/TextJustification.java
@@ -1,45 +1,29 @@
 public class TextJustification {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        int len = 0;
-        List<String> current = new ArrayList<>();
         List<String> res = new ArrayList<>();
-        for (int k = 0; k < words.length; k++) {
-            if (len + words[k].length() + current.size() > maxWidth) {
-                int spaces = maxWidth - len;
-                StringBuilder sb = new StringBuilder();
-                sb.append(current.get(0));
-                if (current.size() == 1) {
-                    for (int i = sb.length(); i < maxWidth; i++) sb.append(' ');
-                } else {
-                    int eachSpace = spaces / (current.size() - 1);
-                    int added = spaces % (current.size() - 1);
-                    for (int i = 1; i < current.size(); i++) {
-                        for (int j = 0; j < eachSpace; j++) {
-                            sb.append(' ');
-                        }
-                        if (i <= added) sb.append(' ');
-                        sb.append(current.get(i));
-                    }
-                }
-                res.add(sb.toString());
-                current.clear();
-                current.add(words[k]);
-                len = words[k].length();
-            } else {
-                len += words[k].length();
-                current.add(words[k]);
+        for (int i = 0, j = 0, len = 0; i < words.length; i += j) {
+            for (j = 0, len = 0; i + j < words.length && len + words[i + j].length() + j <= maxWidth; j++) {
+                len += words[i + j].length();
             }
-        }
-        if (current.size() > 0) {
             StringBuilder sb = new StringBuilder();
-            sb.append(current.get(0));
-            for (int i = 1; i < current.size(); i++) {
-                sb.append(' ');
-                sb.append(current.get(i));
+            sb.append(words[i]);
+            for (int k = 1; k < j; k++) {
+                if (i + j >= words.length) sb.append(' ');
+                else {
+                    int space = (maxWidth - len) / (j - 1);
+                    if (k <= (maxWidth - len) % (j - 1)) space++;
+                    sb.append(makeString(' ', space));
+                }
+                sb.append(words[i + k]);
             }
-            for (int i = sb.length(); i < maxWidth; i++) sb.append(' ');
+            sb.append(makeString(' ', maxWidth - sb.length()));
             res.add(sb.toString());
         }
         return res;
+    }
+    String makeString(char c, int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) sb.append(c);
+        return sb.toString();
     }
 }
