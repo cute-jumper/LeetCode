76b1e915bb655fb28c476197d3adf8896872e549
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-14 23:11:16 -0400

    Update product of array except self

--- Java/201-250/ProductOfArrayExceptSelf.java
+++ Java/201-250/ProductOfArrayExceptSelf.java
@@ -1,17 +1,16 @@
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        int[] output = new int[nums.length];
-        output[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            output[i] = output[i - 1] * nums[i];
+        int[] res = new int[nums.length];
+        int prod = 1;
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = prod;
+            prod *= nums[i];
         }
-        int product = nums[nums.length - 1];
-        output[nums.length - 1] = output[nums.length - 2];
-        for (int i = nums.length - 2; i > 0; i--) {
-            output[i] = product * output[i - 1];
-            product *= nums[i];
+        prod = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= prod;
+            prod *= nums[i];
         }
-        output[0] = product;
-        return output;
+        return res;
     }
 }
