f2d2969f6494d714e88d617c3a4f9fe887a7e005
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-15 23:00:02 -0400

    Update HIndex 2

--- Java/251-300/HIndex2.java
+++ Java/251-300/HIndex2.java
@@ -1,18 +1,12 @@
 public class HIndex2 {
     public int hIndex(int[] citations) {
-        int low = 0;
-        int high = citations.length - 1;
-        int maxH = 0;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            int h = citations.length - mid;
-            if (citations[mid] >= h) {
-                maxH = h;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        int len = citations.length;
+        int low = 0, high = len - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (citations[mid] >= len - mid) high = mid - 1;
+            else low = mid + 1;
         }
-        return maxH;
+        return len - low;
     }
 }
