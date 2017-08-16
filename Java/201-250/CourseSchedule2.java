a86f285585bb556ecd3beedb8240179ba9322223
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-15 20:01:10 -0400

    Update course schedule 2 using queue

--- Java/201-250/CourseSchedule2.java
+++ Java/201-250/CourseSchedule2.java
@@ -1,48 +1,27 @@
 public class CourseSchedule2 {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < prerequisites.length; i++) {
-            int c1 = prerequisites[i][0];
-            int c2 = prerequisites[i][1];
-            if (graph.containsKey(c1)) {
-                graph.get(c1).add(c2);
-            } else {
-                List<Integer> neighbors = new ArrayList<>();
-                neighbors.add(c2);
-                graph.put(c1, neighbors);
-            }
+        int[] indegree = new int[numCourses];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] prereq : prerequisites) {
+            indegree[prereq[0]]++;
+            if (!map.containsKey(prereq[1])) map.put(prereq[1], new ArrayList<>());
+            map.get(prereq[1]).add(prereq[0]);
         }
-        Set<Integer> prefix = new HashSet<>();
-        boolean[] visited = new boolean[numCourses];
-        List<Integer> res = new ArrayList<>();
+        Deque<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < numCourses; i++) {
-            if (!visited[i] && !dfs(i, graph, prefix, visited, res))
-                return new int[0];
+            if (indegree[i] == 0) queue.offer(i);
         }
-        int[] resArr = new int[numCourses];
+        int[] res = new int[numCourses];
         int index = 0;
-        for (Integer i : res) {
-            resArr[index] = i;
-            index++;
-        }
-        return resArr;
-    }
-    public boolean dfs(int n, Map<Integer, List<Integer>> graph, Set<Integer> prefix,
-                    boolean[] visited, List<Integer> res) {
-        if (prefix.contains(n)) return false;
-        List<Integer> neighbors = graph.get(n);
-        if (neighbors != null) {
-            for (Integer i : neighbors) {
-                if (!visited[i]) {
-                    prefix.add(n);
-                    boolean success = dfs(i, graph, prefix, visited, res);
-                    prefix.remove(n);
-                    if (!success) return false;
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            res[index++] = course;
+            if (map.containsKey(course)) {
+                for (Integer i : map.get(course)) {
+                    if (indegree[i]-- == 1) queue.offer(i);
                 }
             }
         }
-        res.add(n);
-        visited[n] = true;
-        return true;
+        return index == res.length ? res : new int[0];
     }
 }
