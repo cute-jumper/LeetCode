2e157662601c86516a828a027cab15a57d85bf01
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-15 20:24:44 -0400

    Update reverse nodes in K group with the recursive method

--- Java/1-50/ReverseNodesInKGroup.java
+++ Java/1-50/ReverseNodesInKGroup.java
@@ -9,29 +9,42 @@
 public class ReverseNodesInKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode node = head;
-        int count = 0;
-        while (node != null) {
-            count++;
+        for (int i = 0; i < k; i++) {
+            if (node == null) return head;
             node = node.next;
         }
-        node = head;
-        ListNode prev = null;
-        ListNode vhead = new ListNode(0);
-        vhead.next = head;
-        ListNode prevEnd = vhead;
-        for (int i = 0; i < count / k; i++) {
-            for (int j = 0; j < k; j++) {
-                ListNode next = node.next;
-                node.next = prev;
-                prev = node;
-                node = next;
-            }
-            ListNode tail = prevEnd.next;
-            prevEnd.next = prev;
-            tail.next = node;
-            prevEnd = tail;
-            prev = null;
+        node = reverseKGroup(node, k);
+        for (int i = 0; i < k; i++) {
+            ListNode next = head.next;
+            head.next = node;
+            node = head;
+            head = next;
         }
-        return vhead.next;
+        return node;
+        // ListNode node = head;
+        // int count = 0;
+        // while (node != null) {
+        //     count++;
+        //     node = node.next;
+        // }
+        // node = head;
+        // ListNode prev = null;
+        // ListNode vhead = new ListNode(0);
+        // vhead.next = head;
+        // ListNode prevEnd = vhead;
+        // for (int i = 0; i < count / k; i++) {
+        //     for (int j = 0; j < k; j++) {
+        //         ListNode next = node.next;
+        //         node.next = prev;
+        //         prev = node;
+        //         node = next;
+        //     }
+        //     ListNode tail = prevEnd.next;
+        //     prevEnd.next = prev;
+        //     tail.next = node;
+        //     prevEnd = tail;
+        //     prev = null;
+        // }
+        // return vhead.next;
     }
 }
