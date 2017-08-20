a8765d90f07bf3d87b6be6db0a63170ed79db703
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-20 16:15:01 -0400

    update add binary

--- Java/51-100/AddBinary.java
+++ Java/51-100/AddBinary.java
@@ -1,21 +1,12 @@
 public class AddBinary {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        int lenA = a.length(), lenB = b.length();
-        int carry = 0;
-        for (int i = 0; i < lenA || i < lenB; i++) {
-            int c1 = 0, c2 = 0;
-            if (i < lenA) {
-                c1 = a.charAt(lenA - 1 - i) - '0';
-            }
-            if (i < lenB) {
-                c2 = b.charAt(lenB - 1 - i) - '0';
-            }
-            int s = c1 + c2 + carry;
-            sb.append(s & 1);
-            carry = (s >> 1) & 1;
+        for (int i = a.length() - 1, j = b.length() - 1, c = 0; i >= 0 || j >= 0 || c != 0; i--, j--) {
+            if (i >= 0) c += a.charAt(i) - '0';
+            if (j >= 0) c += b.charAt(j) - '0';
+            sb.append(c & 1);
+            c >>= 1;
         }
-        if (carry > 0) sb.append(carry);
         return sb.reverse().toString();
     }
 }
