12fe520f610bb1bfedbba834351dfc42e5e385e3
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-20 17:56:10 -0400

    Update island perimeter

--- Java/451-500/IslandPerimeter.java
+++ Java/451-500/IslandPerimeter.java
@@ -1,35 +1,16 @@
 public class IslandPerimeter {
     public int islandPerimeter(int[][] grid) {
         if (grid.length == 0 || grid[0].length == 0) return 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int island = 0, connected = 0;
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
-                    return dfs(grid, visited, i * grid[0].length + j);
+                    island++;
+                    if (i > 0 && grid[i - 1][j] == 1) connected++;
+                    if (j > 0 && grid[i][j - 1] == 1) connected++;
                 }
             }
         }
-        return 0;
-    }
-    public int dfs(int[][] grid, boolean[][] visited, int start) {
-        int perimeter = 0;
-        int height = grid.length;
-        int width = grid[0].length;
-        int startW = start % width;
-        int startH = start / width;
-        visited[startH][startW] = true;
-        int[][] dirs = new int[][] {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        for (int i = 0; i < 4; i++) {
-            int w = startW + dirs[i][0];
-            int h = startH + dirs[i][1];
-            if (w >= 0 && w < width && h >= 0 && h < height && grid[h][w] == 1) {
-                if (!visited[h][w]) {
-                    perimeter += dfs(grid, visited, h * width + w);
-                }
-            } else {
-                perimeter++;
-            }
-        }
-        return perimeter;
+        return 4 * island - 2 * connected;
     }
 }
