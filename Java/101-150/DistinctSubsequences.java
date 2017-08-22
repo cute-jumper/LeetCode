21e7a7c7a8ba18e7269509d666fc22b200d8e846
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-21 20:12:46 -0400

    Add naive dp for distinct subsequences

--- Java/101-150/DistinctSubsequences.java
+++ Java/101-150/DistinctSubsequences.java
@@ -14,5 +14,15 @@ public class DistinctSubsequences {
             }
         }
         return dp[s.length()];
+        // if (s.length() < t.length()) return 0;
+        // int[][] dp = new int[t.length() + 1][s.length() + 1];
+        // Arrays.fill(dp[0], 1);
+        // for (int i = 0; i < t.length(); i++) {
+        //     for (int j = 0; j < s.length(); j++) {
+        //         dp[i + 1][j + 1] = dp[i + 1][j];
+        //         if (t.charAt(i) == s.charAt(j)) dp[i + 1][j + 1] += dp[i][j];
+        //     }
+        // }
+        // return dp[t.length()][s.length()];
     }
 }
