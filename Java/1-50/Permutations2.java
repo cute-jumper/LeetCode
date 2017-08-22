19bc2bdf03068f818053bba78295fc2e8e732f5c
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-21 21:17:49 -0400

    Update permutations 2

--- Java/1-50/Permutations2.java
+++ Java/1-50/Permutations2.java
@@ -1,26 +1,24 @@
 public class Permutations2 {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Arrays.sort(nums);
-        return genPermutations(nums, new boolean[nums.length], new ArrayList<Integer>());
-    }
-    public List<List<Integer>> genPermutations(int[] nums, boolean[] flags, ArrayList<Integer> prefix) {
         List<List<Integer>> res = new ArrayList<>();
-        if (prefix.size() == nums.length) {
-            res.add(new ArrayList<Integer>(prefix));
-            return res;
-        }
-        int prev = nums[0] - 1;
-        for (int i = 0; i < flags.length; i++) {
-            if (flags[i]) continue;
-            if (nums[i] == prev) continue;
-            flags[i] = true;
-            prefix.add(nums[i]);
-            prev = nums[i];
-            List<List<Integer>> perms = genPermutations(nums, flags, prefix);
-            res.addAll(perms);
-            flags[i] = false;
-            prefix.remove(prefix.size() - 1);
-        }
+        Arrays.sort(nums);
+        dfs(res, new ArrayList<>(), nums, new boolean[nums.length], nums.length);
         return res;
+    }
+    void dfs(List<List<Integer>> res, List<Integer> curr, int[] nums, boolean[] visited, int remain) {
+        if (remain == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = 0, prev = nums[0] - 1; i < nums.length; i++) {
+            if (!visited[i] && nums[i] != prev) {
+                visited[i] = true;
+                curr.add(nums[i]);
+                dfs(res, curr, nums, visited, remain - 1);
+                curr.remove(curr.size() - 1);
+                visited[i] = false;
+                prev = nums[i];
+            }
+        }
     }
 }
