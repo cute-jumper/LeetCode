8b8f10907e3b640a40cfce2824fd9cfb9f67e2eb
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-21 21:09:50 -0400

    Update permutations

--- Java/1-50/Permutations.java
+++ Java/1-50/Permutations.java
@@ -1,22 +1,22 @@
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
-        return genPermutations(nums, new boolean[nums.length], new ArrayList<>());
-    }
-    public List<List<Integer>> genPermutations(int[] nums, boolean[] flags, List<Integer> prefix) {
         List<List<Integer>> res = new ArrayList<>();
-        if (prefix.size() == nums.length) {
-            res.add(new ArrayList<Integer>(prefix));
-            return res;
-        }
-        for (int i = 0; i < flags.length; i++) {
-            if (flags[i]) continue;
-            flags[i] = true;
-            prefix.add(nums[i]);
-            List<List<Integer>> perms = genPermutations(nums, flags, prefix);
-            res.addAll(perms);
-            flags[i] = false;
-            prefix.remove(prefix.size() - 1);
-        }
+        permute(res, new ArrayList<>(), nums, new boolean[nums.length], nums.length);
         return res;
+    }
+    void permute(List<List<Integer>> res, List<Integer> curr, int[] nums, boolean[] visited, int remain) {
+        if (remain == 0) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                curr.add(nums[i]);
+                visited[i] = true;
+                permute(res, curr, nums, visited, remain - 1);
+                visited[i] = false;
+                curr.remove(curr.size() - 1);
+            }
+        }
     }
 }
