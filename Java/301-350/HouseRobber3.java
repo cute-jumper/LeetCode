030e76a9e6e6962ea7beeb0e88c91c9b4aa1dcc1
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-22 00:12:02 -0400

    Update house robber 3

--- Java/301-350/HouseRobber3.java
+++ Java/301-350/HouseRobber3.java
@@ -8,25 +8,14 @@
  * }
  */
 public class HouseRobber3 {
-    static class Pair {
-        int val1;
-        int val2;
-        public Pair(int val1, int val2) {
-            this.val1 = val1;
-            this.val2 = val2;
-        }
-        public int maxVal() {
-            return Math.max(val1, val2);
-        }
-    }
     public int rob(TreeNode root) {
-        return maxTree(root).maxVal();
+        int[] res = helper(root);
+        return Math.max(res[0], res[1]);
     }
-    public Pair maxTree(TreeNode root) {
-        if (root == null) return new Pair(0, 0);
-        Pair left = maxTree(root.left);
-        Pair right = maxTree(root.right);
-        return new Pair(root.val + left.val2 + right.val2,
-                        left.maxVal() + right.maxVal());
+    int[] helper(TreeNode root) {
+        if (root == null) return new int[] { 0, 0 };
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+        return new int[] { root.val + left[1] + right[1], Math.max(left[0], left[1]) + Math.max(right[0], right[1]) };
     }
 }
