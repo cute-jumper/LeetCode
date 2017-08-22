fb2afd3034e8d8b1564ecc674a64c77037e4194b
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-22 14:38:37 -0400

    Update tasks scheduler

--- Java/601-650/TaskScheduler.java
+++ Java/601-650/TaskScheduler.java
@@ -1,41 +1,26 @@
 public class TaskScheduler {
-        static class Task implements Comparable<Task> {
-        int freq;
-        Task(int freq) {
-            this.freq = freq;
-        }
-        @Override
-        public int compareTo(Task o) {
-            return o.freq - freq;
-        }
-    }
     public int leastInterval(char[] tasks, int n) {
         Map<Character, Integer> map = new HashMap<>();
-        for (char i : tasks) {
-            if (map.containsKey(i)) map.put(i, map.get(i) + 1);
-            else map.put(i, 1);
+        for (char c : tasks) {
+            if (map.containsKey(c)) map.put(c, map.get(c) + 1);
+            else map.put(c, 1);
         }
-        PriorityQueue<Task> queue = new PriorityQueue<>();
-        for (Character key : map.keySet()) {
-            queue.offer(new Task(map.get(key)));
-        }
-        Deque<Task> waitQueue = new ArrayDeque<>();
-        int count = tasks.length;
-        int ticks = 0;
-        while (count > 0) {
-            ticks++;
-            if (queue.isEmpty()) waitQueue.offer(new Task(-1));
-            else {
-                Task t = queue.poll();
-                t.freq--;
-                count--;
-                waitQueue.offer(t);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int freq : map.values()) pq.offer(-freq);
+        Deque<Integer> queue = new ArrayDeque<>();
+        int cycle = 0;
+        for (int i = 0; i < tasks.length; cycle++) {
+            if (pq.isEmpty()) {
+                queue.offer(1);
+            } else {
+                queue.offer(pq.poll() + 1);
+                i++;
             }
-            if (waitQueue.size() > n) {
-                Task t = waitQueue.poll();
-                if (t.freq > 0) queue.offer(t);
+            if (queue.size() > n) {
+                int freq = queue.poll();
+                if (freq < 0) pq.offer(freq);
             }
         }
-        return ticks;
+        return cycle;
     }
 }
