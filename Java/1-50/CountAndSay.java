448ea849126e67ff8d9f0547b4ceb4027381d9e1
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-22 17:41:50 -0400

    Update count and say

--- Java/1-50/CountAndSay.java
+++ Java/1-50/CountAndSay.java
@@ -1,27 +1,22 @@
 public class CountAndSay {
     public String countAndSay(int n) {
-        StringBuilder sb = new StringBuilder("1");
-        while (n > 1) {
-            String s = sb.toString();
-            sb = new StringBuilder();
-            char prev = s.charAt(0);
-            int count = 1;
-            for (int i = 1; i < s.length(); i++) {
-                if (s.charAt(i) == prev) {
-                    count++;
-                } else {
-                    sb.append(count);
-                    sb.append(prev);
-                    prev = s.charAt(i);
-                    count = 1;
-                }
+        String s = "1";
+        for (int i = 1; i < n; i++) s = next(s);
+        return s;
+    }
+    String next(String s) {
+        int count = 0;
+        char c = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (count == 0 || s.charAt(i) != c) {
+                if (count > 0) sb.append(count + "" + c);
+                c = s.charAt(i);
+                count = 1;
+            } else {
+                count++;
             }
-            if (count != 0) {
-                sb.append(count);
-                sb.append(prev);
-            }
-            n--;
         }
-        return sb.toString();
+        return sb.toString() + count + c;
     }
 }
