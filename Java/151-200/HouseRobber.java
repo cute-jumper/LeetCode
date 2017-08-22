9df1a65ed7722c70fad3b83103497bf1e6db3f6a
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-22 00:00:53 -0400

    Update house robber

--- Java/151-200/HouseRobber.java
+++ Java/151-200/HouseRobber.java
@@ -1,12 +1,14 @@
 public class HouseRobber {
     public int rob(int[] nums) {
         if (nums.length == 0) return 0;
-        int[] dp = new int[nums.length + 1];
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for (int i = 2; i <= nums.length; i++) {
-            dp[i] = Math.max(nums[i - 1] + dp[i - 2], dp[i-1]);
+        int first = 0, second = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                first = Math.max(second, first + nums[i]);
+            } else {
+                second = Math.max(first, second + nums[i]);
+            }
         }
-        return dp[nums.length];
+        return Math.max(first, second);
     }
 }
