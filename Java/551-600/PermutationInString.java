95eb9b8b15b1dae8c1718083be19b3d114d50846
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-23 16:24:22 -0400

    Update permutations in string

--- Java/551-600/PermutationInString.java
+++ Java/551-600/PermutationInString.java
@@ -1,33 +1,12 @@
 public class PermutationInString {
     public boolean checkInclusion(String s1, String s2) {
-        int[] stat = new int[256];
-        boolean[] shown = new boolean[256];
-        int total = 0;
-        int len = s1.length();
-        for (int i = 0; i < len; i++) {
-            char c = s1.charAt(i);
-            stat[c]++;
-            if (!shown[c]) {
-                total++;
-                shown[c] = true;
-            }
-        }
+        int[] count = new int[256];
+        for (int i = 0; i < s1.length(); i++) count[s1.charAt(i)]++;
+        int start = 0, remain = s1.length();
         for (int i = 0; i < s2.length(); i++) {
-            char c = s2.charAt(i);
-            if (shown[c]) {
-                int v = --stat[c];
-                if (v == 0) total--;
-                else if (v == -1) total++;
-            }
-            if (i >= len) {
-                c = s2.charAt(i - len);
-                if (shown[c]) {
-                    int v = ++stat[c];
-                    if (v == 0) total--;
-                    else if (v == 1) total++;
-                }
-            }
-            if (total == 0) return true;
+            if (count[s2.charAt(i)]-- >= 1) remain--;
+            if (remain == 0) return true;
+            if (i - start + 1 == s1.length() && count[s2.charAt(start++)]++ >= 0) remain++;
         }
         return false;
     }
