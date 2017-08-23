f7ea9897ec9617b632a230ab1105d262dc8857b6
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-23 14:57:55 -0400

    Update largest number

--- Java/151-200/LargestNumber.java
+++ Java/151-200/LargestNumber.java
@@ -1,24 +1,16 @@
 public class LargestNumber {
     public String largestNumber(int[] nums) {
-        List<Integer> numList = new ArrayList<>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            numList.add(nums[i]);
-        }
-        Collections.sort(numList, new Comparator<Integer>() {
-               @Override
-               public int compare(Integer a, Integer b) {
-                   String as = a + "" + b;
-                   String bs = b + "" + a;
-                   return bs.compareTo(as);
-               }
+        List<String> numList = new ArrayList<>(nums.length);
+        for (int i = 0; i < nums.length; i++) numList.add(nums[i] + "");
+        Collections.sort(numList, new Comparator<String>() {
+                @Override
+                public int compare(String a, String b) {
+                    return (b + a).compareTo(a + b);
+                }
             });
+        if (numList.isEmpty() || numList.get(0).equals("0")) return "0";
         StringBuilder sb = new StringBuilder();
-        if (numList.size() > 1 && numList.get(0) == 0 &&
-            numList.get(numList.size() - 1) == 0)
-            return "0";
-        for (Integer i : numList) {
-            sb.append(i);
-        }
+        for (String i : numList) sb.append(i);
         return sb.toString();
     }
 }
