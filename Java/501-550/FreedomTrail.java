0ed206d56f08f7cd37f57840a5e2af71608e7356
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-23 13:18:17 -0400

    Update freedom trail

--- Java/501-550/FreedomTrail.java
+++ Java/501-550/FreedomTrail.java
@@ -1,18 +1,17 @@
 public class FreedomTrail {
     public int findRotateSteps(String ring, String key) {
         int[][] memo = new int[ring.length()][key.length()];
-        return dfs(memo, ring, 0, key, 0, 0);
+        return dfs(memo, ring, 0, key, 0);
     }
-    public int dfs(int[][] memo, String ring, int start, String key, int curr, int dist) {
-        if (curr == key.length()) return dist;
-        if (memo[start][curr] != 0) return memo[start][curr] + dist;
+    public int dfs(int[][] memo, String ring, int start, String key, int curr) {
+        if (curr == key.length()) return 0;
+        if (memo[start][curr] != 0) return memo[start][curr];
         char c = key.charAt(curr);
         int f = forward(ring, start, c);
         int b = backward(ring, start, c);
-        int minStep = Math.min(dfs(memo, ring, (start + f) % ring.length(), key, curr + 1, dist + f + 1),
-            dfs(memo, ring, (start - b + ring.length()) % ring.length(), key, curr + 1, dist + b + 1));
-        memo[start][curr] = minStep - dist;
-        return minStep;
+        memo[start][curr] = Math.min(dfs(memo, ring, (start + f) % ring.length(), key, curr + 1) + f + 1,
+                                     dfs(memo, ring, (start - b + ring.length()) % ring.length(), key, curr + 1) + b + 1);
+        return memo[start][curr];
     }
     public int forward(String ring, int start, char c) {
         for (int i = start, cnt = 0; cnt < ring.length(); cnt++, i++) {
