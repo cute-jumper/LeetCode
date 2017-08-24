3eedfa22ce734c116057ea38ea344f8b29b2614d
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-24 12:25:00 -0400

    Update search in rotated sorted array 2

--- Java/51-100/SearchInRotatedSortedArray2.java
+++ Java/51-100/SearchInRotatedSortedArray2.java
@@ -1,37 +1,20 @@
 public class SearchInRotatedSortedArray2 {
     public boolean search(int[] nums, int target) {
         if (nums.length == 0) return false;
-        int low = 0;
-        int high = nums.length - 1;
-        if (target == nums[0]) return true;
-        if (nums[0] == nums[nums.length - 1]) {
-            while (low <= high && nums[low] == nums[0]) low++;
-            while (high >= low && nums[high] == nums[0]) high--;
-            if (low > high) return false;
-            if (low == high)
-                return nums[low] == target;
-        }
-        int type = 0;
-        int start = nums[low];
-        int end = nums[high];
-        if (nums[low] > nums[high]) {
-            if (target >= nums[low]) type = 1;
-            else if (target <= nums[high]) type = 2;
-            else return false;
-        }
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
             if (nums[mid] == target) return true;
-            if (type == 1 && nums[mid] <= end) {
-                high = mid - 1;
-            } else if (type == 2 && nums[mid] >= start) {
-                low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
+            if (nums[mid] < nums[high]) {
+                if (nums[mid] < target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            } else if (nums[mid] > nums[high]) {
+                if (nums[mid] > target && nums[low] <= target) high = mid - 1;
+                else low = mid + 1;
             } else {
-                low = mid + 1;
+                high--;
             }
         }
-        return false;
+        return nums[low] == target;
     }
 }
