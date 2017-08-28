7b5593d9102eb34ab42f1ce071d02e31d34d382d
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-27 21:19:46 -0400

    Update verify preorder of binary tree

--- Java/301-350/VerifyPreorderOfBST.java
+++ Java/301-350/VerifyPreorderOfBST.java
@@ -1,22 +1,12 @@
 public class VerifyPreorderOfBST {
     public boolean isValidSerialization(String preorder) {
-        if (preorder.equals("#")) return true;
-        String[] nodes = preorder.split(",");
-        int[] stack = new int[nodes.length];
+        int[] stack = new int[preorder.length()];
         int top = -1;
+        String[] nodes = preorder.split(",");
         for (int i = 0; i < nodes.length; i++) {
             if (nodes[i].equals("#")) {
-                if (top < 0) return false;
-                stack[top]++;
-                while (stack[top] == 2) {
-                    stack[top] = 0;
-                    top--;
-                    if (top == -1) {
-                        return i == nodes.length - 1;
-                    } else {
-                        stack[top]++;
-                    }
-                }
+                while (top >= 0 && stack[top]++ == 1) stack[top--] = 0;
+                if (top == -1) return i == nodes.length - 1;
             } else {
                 top++;
             }
