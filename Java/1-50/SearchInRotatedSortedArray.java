d8ff203b8c068d3fe5d1286dce9d7892b188ef94
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-29 14:34:37 -0400

    Update search in rotated sorted array

--- Java/1-50/SearchInRotatedSortedArray.java
+++ Java/1-50/SearchInRotatedSortedArray.java
@@ -1,18 +1,17 @@
 public class SearchInsertPosition {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) return -1;
         int low = 0, high = nums.length - 1;
-        while (low < high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] == target) return mid;
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) high = mid - 1;
-                else low = mid + 1;
-            } else {
+            if (nums[mid] < nums[high]) {
                 if (nums[mid] < target && target <= nums[high]) low = mid + 1;
                 else high = mid - 1;
+            } else {
+                if (nums[mid] > target && target >= nums[low]) high = mid - 1;
+                else low = mid + 1;
             }
         }
-        return nums[low] == target ? low : -1;
+        return -1;
     }
 }
