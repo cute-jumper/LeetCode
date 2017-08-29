5d3930217ca84225192c027fa94a4e93837af7e2
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-29 13:53:46 -0400

    Update minimum size subarray sum

--- Java/201-250/MinimumSizeSubarraySum.java
+++ Java/201-250/MinimumSizeSubarraySum.java
@@ -1,16 +1,13 @@
 public class MinimumSizeSubarraySum {
     public int minSubArrayLen(int s, int[] nums) {
-        int start = 0;
-        int minLen = nums.length + 1;
-        int sum = 0;
+        int min = nums.length + 1, sum = 0, start = 0;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             while (sum >= s) {
-                minLen = Math.min(minLen, i - start + 1);
-                sum -= nums[start];
-                start++;
+                min = Math.min(min, i - start + 1);
+                sum -= nums[start++];
             }
         }
-        return minLen == nums.length + 1 ? 0 : minLen;
+        return min % (nums.length + 1);
     }
 }
