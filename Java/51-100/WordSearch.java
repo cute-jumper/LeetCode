20bc86bdee7a09423f7b228ad6e7c9016dd2db84
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-29 13:35:37 -0400

    Update word search

--- Java/51-100/WordSearch.java
+++ Java/51-100/WordSearch.java
@@ -1,42 +1,24 @@
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
-        if (board.length == 0 || board[0].length == 0) return false;
-        int row = board.length;
-        int column = board[0].length;
-        boolean[][] flags = new boolean[board.length][board[0].length];
-        char c = word.charAt(0);
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                if (board[i][j] == c) {
-                    flags[i][j] = true;
-                    boolean match = wordSearch(board, word, 1, j, i, flags);
-                    if (match) return true;
-                    flags[i][j] = false;
-                }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (search(board, i, j, word, 0, new boolean[board.length][board[0].length])) return true;
             }
         }
         return false;
     }
-    public boolean wordSearch(char[][] board, String word, int start,
-                              int x, int y, boolean[][] flags) {
-        if (start == word.length()) {
-            return true;
-        }
-        int[] xDirs = new int[] {-1, 0, 1, 0};
-        int[] yDirs = new int[] {0, -1, 0, 1};
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + xDirs[i];
-            int nextY = y + yDirs[i];
-            if (nextX >= 0 && nextX < board[0].length &&
-                nextY >=0 && nextY < board.length &&
-                board[nextY][nextX] == word.charAt(start) &&
-                !flags[nextY][nextX]) {
-                flags[nextY][nextX] = true;
-                boolean match = wordSearch(board, word, start + 1, nextX, nextY, flags);
-                if (match) return true;
-                flags[nextY][nextX] = false;
+    boolean search(char[][] board, int row, int col, String word, int start, boolean[][] visited) {
+        if (board[row][col] != word.charAt(start)) return false;
+        if (start == word.length() - 1) return true;
+        visited[row][col] = true;
+        int[][] dirs = new int[][] {{ 0, -1 }, { 0, 1 }, { 1, 0 }, { -1, 0 }};
+        for (int[] dir : dirs) {
+            int r = row + dir[0], c = col + dir[1];
+            if (r >= 0 && r < board.length && c >= 0 && c < board[r].length && !visited[r][c]) {
+                if (search(board, r, c, word, start + 1, visited)) return true;
             }
         }
+        visited[row][col] = false;
         return false;
     }
 }
