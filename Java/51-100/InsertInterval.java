c0b350f275e04646d51763fb92196146a7cccef5
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-29 13:48:27 -0400

    Update insert interval

--- Java/51-100/InsertInterval.java
+++ Java/51-100/InsertInterval.java
@@ -11,17 +11,13 @@ public class InsertInterval {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> res = new ArrayList<>();
         int index = 0;
-        for (; index < intervals.size() && intervals.get(index).end < newInterval.start; index++) {
-            res.add(intervals.get(index));
-        }
-        if (index < intervals.size()) newInterval.start = Math.min(newInterval.start, intervals.get(index).start);
+        for (; index < intervals.size() && intervals.get(index).end < newInterval.start; index++) res.add(intervals.get(index));
         for (; index < intervals.size() && intervals.get(index).start <= newInterval.end; index++) {
+            newInterval.start = Math.min(newInterval.start, intervals.get(index).start);
             newInterval.end = Math.max(newInterval.end, intervals.get(index).end);
         }
         res.add(newInterval);
-        for (; index < intervals.size(); index++) {
-            res.add(intervals.get(index));
-        }
+        for (; index < intervals.size(); index++) res.add(intervals.get(index));
         return res;
     }
 }
