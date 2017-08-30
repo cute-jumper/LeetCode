966c4f8f87717cb50890a098bce696264bb10720
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-30 14:37:34 -0400

    Update task scheduler using idle slots

--- Java/601-650/TaskScheduler.java
+++ Java/601-650/TaskScheduler.java
@@ -1,26 +1,12 @@
 public class TaskScheduler {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : tasks) {
-            if (map.containsKey(c)) map.put(c, map.get(c) + 1);
-            else map.put(c, 1);
+        int[] count = new int[256];
+        int max = 0;
+        for (char c : tasks) max = Math.max(max, count[c]++);
+        int idle = max * (n + 1);
+        for (int i : count) {
+            if (i > 0) idle -= Math.min(max, i);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int freq : map.values()) pq.offer(-freq);
-        Deque<Integer> queue = new ArrayDeque<>();
-        int cycle = 0;
-        for (int i = 0; i < tasks.length; cycle++) {
-            if (pq.isEmpty()) {
-                queue.offer(1);
-            } else {
-                queue.offer(pq.poll() + 1);
-                i++;
-            }
-            if (queue.size() > n) {
-                int freq = queue.poll();
-                if (freq < 0) pq.offer(freq);
-            }
-        }
-        return cycle;
+        return tasks.length + (idle > 0 ? idle : 0);
     }
 }
