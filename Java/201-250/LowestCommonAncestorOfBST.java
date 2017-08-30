14df06ed287e8af73f8ec76dce2d026aee9b3f2f
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-29 21:21:01 -0400

    Update lowest common ancestor of BST

--- Java/201-250/LowestCommonAncestorOfBST.java
+++ Java/201-250/LowestCommonAncestorOfBST.java
@@ -10,13 +10,9 @@
 public class LowestCommonAncestorOfBST {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || p == null || q == null) return null;
-        int minValue = Math.min(p.val, q.val);
-        int maxValue = Math.max(p.val, q.val);
-        if (root.val < minValue) {
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (root.val > maxValue) {
-            return lowestCommonAncestor(root.left, p, q);
-        }
+        int minValue = Math.min(p.val, q.val), maxValue = Math.max(p.val, q.val);
+        if (root.val < minValue) return lowestCommonAncestor(root.right, p, q);
+        if (root.val > maxValue) return lowestCommonAncestor(root.left, p, q);
         return root;
     }
 }
