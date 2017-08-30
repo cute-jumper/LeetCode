7d53ba4a079c7eb169c8832084709a18398b3895
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-30 14:38:17 -0400

    Update tasks scheduler with pq in comment

--- Java/601-650/TaskScheduler.java
+++ Java/601-650/TaskScheduler.java
@@ -8,5 +8,27 @@ public class TaskScheduler {
             if (i > 0) idle -= Math.min(max, i);
         }
         return tasks.length + (idle > 0 ? idle : 0);
+        // int[] count = new int[256];
+        // for (char c : tasks) count[c]++;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // Deque<Integer> waitQueue = new ArrayDeque<>();
+        // for (int i : count) {
+        //     if (i != 0) pq.offer(i);
+        // }
+        // int cycle = tasks.length;
+        // for (int i = 0; i < tasks.length;) {
+        //     if (pq.isEmpty()) {
+        //         waitQueue.offer(-1);
+        //         cycle++;
+        //     } else {
+        //         waitQueue.offer(pq.poll() - 1);
+        //         i++;
+        //     }
+        //     if (waitQueue.size() > n) {
+        //         int freq = waitQueue.poll();
+        //         if (freq > 0) pq.offer(freq);
+        //     }
+        // }
+        // return cycle;
     }
 }
