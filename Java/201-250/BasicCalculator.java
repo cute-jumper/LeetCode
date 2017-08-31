7a2254b0e1b2a9099a9af1dea78b95a03d1d573f
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-31 14:12:30 -0400

    Update basic calculator

--- Java/201-250/BasicCalculator.java
+++ Java/201-250/BasicCalculator.java
@@ -1,29 +1,27 @@
 public class BasicCalculator {
     public int calculate(String s) {
+        int res = 0, num = 0, sign = 1;
         Deque<Integer> stack = new ArrayDeque<>();
-        int sign = 1;
-        int num = 0;
-        int curr = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '+' || c == '-') {
-                curr += sign * num;
-                num = 0;
-                sign = c == '+' ? 1 : -1;
-            } else if ('0' <= c && c <= '9') {
-                num = 10 * num + (c - '0');
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
             } else if (c == '(') {
-                stack.push(curr);
+                stack.push(res);
                 stack.push(sign);
-                curr = num = 0;
+                res = num = 0;
                 sign = 1;
             } else if (c == ')') {
-                curr += sign * num;
-                curr *= stack.pop();
-                curr += stack.pop();
+                res = (res + num * sign) * stack.pop();
+                res += stack.pop();
+                num = 0;
+            }
+            if (i == s.length() - 1 || c == '+' || c == '-') {
+                res += num * sign;
+                sign = c == '+' ? 1 : -1;
                 num = 0;
             }
         }
-        if (num > 0) curr += num * sign;
-        return curr;
+        return res;
     }
 }
