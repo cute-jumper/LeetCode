f8d959deca1fe69f04f0b429654f749ba0af480d
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-31 16:24:19 -0400

    Update intersection of two arrays

--- LintCode/IntersectionOfTwoArrays547.java
+++ LintCode/IntersectionOfTwoArrays547.java
@@ -5,22 +5,15 @@ public class IntersectionOfTwoArrays547 {
      * @return an integer array
      */
     public int[] intersection(int[] nums1, int[] nums2) {
-        // Write your code here
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0, j = 0; i < nums1.length && j < nums2.length;) {
-            if (nums1[i] == nums2[j]) {
-                res.add(nums1[i]);
-                while (i < nums1.length - 1 && nums1[i] == nums1[i + 1]) i++;
-                i++;
-                while (j < nums2.length - 1 && nums2[j] == nums2[j + 1]) j++;
-                j++;
-            } else if (nums1[i] < nums2[j]) i++;
-            else j++;
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums1) set.add(i);
+        Set<Integer> intersect = new HashSet<>();
+        for (int i : nums2) {
+            if (set.contains(i)) intersect.add(i);
         }
-        int[] r = new int[res.size()];
-        for (int i = 0; i < r.length; i++) r[i] = res.get(i);
-        return r;
+        int[] res = new int[intersect.size()];
+        Iterator<Integer> it = intersect.iterator();
+        for (int i = 0; i < res.length; i++) res[i] = it.next();
+        return res;
     }
 }
