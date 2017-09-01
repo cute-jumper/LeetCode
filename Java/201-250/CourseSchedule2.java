1ad696d9d19440e50e0b1bbc790d86356313b7e1
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-31 22:32:33 -0400

    Update DFS version for course schedule 2

--- Java/201-250/CourseSchedule2.java
+++ Java/201-250/CourseSchedule2.java
@@ -24,4 +24,33 @@ public class CourseSchedule2 {
         }
         return index == res.length ? res : new int[0];
     }
+    // public int[] findOrder(int numCourses, int[][] prerequisites) {
+    //     Map<Integer, List<Integer>> map = new HashMap<>();
+    //     for (int[] pre : prerequisites) {
+    //         if (!map.containsKey(pre[0])) map.put(pre[0], new ArrayList<>());
+    //         map.get(pre[0]).add(pre[1]);
+    //     }
+    //     boolean[] visited = new boolean[numCourses];
+    //     List<Integer> list = new ArrayList<>();
+    //     for (int i = 0; i < numCourses; i++) {
+    //         if (!visited[i] && !dfs(list, map, i, new boolean[numCourses], visited)) return new int[0];
+    //     }
+    //     int[] res = new int[numCourses];
+    //     for (int i = 0; i < numCourses; i++) res[i] = list.get(i);
+    //     return res;
+    // }
+    // boolean dfs(List<Integer> list, Map<Integer, List<Integer>> map, int n, boolean[] instack, boolean[] visited) {
+    //     if (instack[n]) return false;
+    //     instack[n] = true;
+    //     if (map.containsKey(n)) {
+    //         List<Integer> neighbors = map.get(n);
+    //         for (int i : neighbors) {
+    //             if (!visited[i] && !dfs(list, map, i, instack, visited)) return false;
+    //         }
+    //     }
+    //     instack[n] = false;
+    //     visited[n] = true;
+    //     list.add(n);
+    //     return true;
+    // }
 }
