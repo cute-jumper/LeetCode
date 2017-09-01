7c0d15149d59a5ebd1efbb359838b4f0d040a616
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-31 22:51:00 -0400

    Update search for a range

--- Java/1-50/SearchForARange.java
+++ Java/1-50/SearchForARange.java
@@ -1,27 +1,22 @@
 public class SearchForARange {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int[] { -1, -1 };
-        int low = 0;
-        int high = nums.length - 1;
+        int[] res = new int[] { -1, -1 };
+        if (nums.length == 0) return res;
+        int low = 0, high = nums.length - 1;
         while (low < high) {
             int mid = low + (high - low) / 2;
-            if (target <= nums[mid]) {
-                high = mid;
-            } else {
-                low =  mid + 1;
-            }
+            if (nums[mid] >= target) high = mid;
+            else low = mid + 1;
         }
-        if (nums[low] != target) return new int[] { -1, -1 };
-        int lowerBound = low;
+        if (nums[low] != target) return res;
+        res[0] = low;
         high = nums.length - 1;
         while (low < high) {
-            int mid = low + (high + 1 - low) / 2;
-            if (target < nums[mid]) {
-                high = mid - 1;
-            } else {
-                low =  mid;
-            }
+            int mid = low + (high - low + 1) / 2;
+            if (nums[mid] <= target) low = mid;
+            else high = mid - 1;
         }
-        return new int[] { lowerBound, low };
+        res[1] = low;
+        return res;
     }
 }
