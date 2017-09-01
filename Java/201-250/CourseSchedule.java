9907e847d17a9cea72e0565d9561eee725db2e36
Junpeng Qiu <qjpchmail@gmail.com>
2017-08-31 22:38:52 -0400

    Update DFS for course schedule

--- Java/201-250/CourseSchedule.java
+++ Java/201-250/CourseSchedule.java
@@ -2,36 +2,25 @@ public class CourseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
         boolean[] visited = new boolean[numCourses];
-        Set<Integer> prefix = new HashSet<>();
-        for (int i = 0; i < prerequisites.length; i++) {
-            int c1 = prerequisites[i][0];
-            int c2 = prerequisites[i][1];
-            if (graph.containsKey(c1)) {
-                graph.get(c1).add(c2);
-            } else {
-                List<Integer> preq = new ArrayList<>();
-                preq.add(c2);
-                graph.put(c1, preq);
-            }
+        for (int[] pre : prerequisites) {
+            if (!graph.containsKey(pre[0])) graph.put(pre[0], new ArrayList<>());
+            graph.get(pre[0]).add(pre[1]);
         }
         for (int i = 0; i < numCourses; i++) {
-            if (visited[i]) continue;
-            if (detectCycle(i, graph, prefix, visited)) return false;
+            if (!visited[i] && detectCycle(i, graph, new boolean[numCourses], visited)) return false;
         }
         return true;
     }
-    public boolean detectCycle(int n, Map<Integer, List<Integer>> graph, Set<Integer> prefix, boolean[] visited) {
-        if (prefix.contains(n)) return true;
-        List<Integer> neighbors = graph.get(n);
-        if (neighbors != null) {
-            for (Integer i : neighbors) {
-                if (visited[i]) continue;
-                prefix.add(n);
-                boolean hasCycle = detectCycle(i, graph, prefix, visited);
-                prefix.remove(n);
-                if (hasCycle) return true;
+    boolean detectCycle(int n, Map<Integer, List<Integer>> graph, boolean[] instack, boolean[] visited) {
+        if (instack[n]) return true;
+        instack[n] = true;
+        if (graph.containsKey(n)) {
+            List<Integer> neighbors = graph.get(n);
+            for (int i : neighbors) {
+                if (!visited[i] && detectCycle(i, graph, instack, visited)) return true;
             }
         }
+        instack[n] = false;
         visited[n] = true;
         return false;
     }
