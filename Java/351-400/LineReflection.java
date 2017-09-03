855fad940cbe5bb70e038990b7168692b3772c44
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-03 15:31:49 -0400

    Update line reflection

--- Java/351-400/LineReflection.java
+++ Java/351-400/LineReflection.java
@@ -3,15 +3,14 @@ public class LineReflection {
         if (points.length == 0) return true;
         int sum = 0;
         Map<Integer, Set<Integer>> map = new HashMap<>();
-        int count = 0;
         for (int[] p : points) {
-            if (!map.containsKey(p[0])) map.put(p[0], new HashSet<>());
-            if (map.get(p[0]).add(p[1])) {
+            if (!map.containsKey(p[0])) {
+                map.put(p[0], new HashSet<>());
                 sum += p[0];
-                count++;
             }
+            map.get(p[0]).add(p[1]);
         }
-        int s = 2 * sum / count;
+        int s = 2 * sum / map.size();
         for (int[] p : points) {
             int x = s - p[0];
             if (!map.containsKey(x) || !map.get(x).contains(p[1])) return false;
