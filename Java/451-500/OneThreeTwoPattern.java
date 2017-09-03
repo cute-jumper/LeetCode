b72a98d611de09cdf602925b8e931fe21ec9b026
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-03 14:41:10 -0400

    Update one three two pattern

--- Java/451-500/OneThreeTwoPattern.java
+++ Java/451-500/OneThreeTwoPattern.java
@@ -1,16 +1,10 @@
 public class OneThreeTwoPattern {
     public boolean find132pattern(int[] nums) {
-        if (nums.length < 3) return false;
         Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(nums[nums.length - 1]);
-        int last = Integer.MIN_VALUE;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < last) return true;
-            if (nums[i] > stack.peek()) {
-                while (!stack.isEmpty() && nums[i] > stack.peek()) {
-                    last = stack.pop();
-                }
-            }
+        int mid = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < mid) return true;
+            while (!stack.isEmpty() && nums[i] > stack.peek()) mid = stack.pop();
             stack.push(nums[i]);
         }
         return false;
