2195d5695d5929cb35fba47654eacd329a73931c
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 15:55:21 -0400

    Update split array into consecutive subsequences

--- Java/651-700/SplitArrayIntoConsecutiveSubseq.java
+++ Java/651-700/SplitArrayIntoConsecutiveSubseq.java
@@ -1,30 +1,14 @@
 public class Solution {
     public boolean isPossible(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>(), next = new HashMap<>();
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
         for (int i : nums) {
-            add(map, i);
+            if (!map.containsKey(i)) map.put(i, new PriorityQueue<>());
+            if (map.containsKey(i - 1) && !map.get(i - 1).isEmpty()) map.get(i).offer(map.get(i - 1).poll() + 1);
+            else map.get(i).offer(1);
         }
-        for (int i : nums) {
-            if (map.get(i) <= 0) continue;
-            if (next.containsKey(i) && next.get(i) > 0) {
-                add(next, i + 1);
-                minus(next, i);
-            } else if (map.containsKey(i + 1) && map.get(i + 1) > 0 && map.containsKey(i + 2) && map.get(i + 2) > 0) {
-                minus(map, i + 1);
-                minus(map, i + 2);
-                add(next, i + 3);
-            } else {
-                return false;
-            }
-            minus(map, i);
+        for (PriorityQueue<Integer> q : map.values()) {
+            if (!q.isEmpty() && q.peek() < 3) return false;
         }
         return true;
-    }
-    void add(Map<Integer, Integer> map, int key) {
-        if (map.containsKey(key)) map.put(key, map.get(key) + 1);
-        else map.put(key, 1);
-    }
-    void minus(Map<Integer, Integer> map, int key) {
-        map.put(key, map.get(key) - 1);
     }
 }
