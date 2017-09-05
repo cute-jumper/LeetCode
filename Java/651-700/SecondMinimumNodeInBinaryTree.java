e167f6b2cce7cc359dbc7f9c6ada30c8f6036063
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 14:09:18 -0400

    Add second minimum node in binary tree

---
+++ Java/651-700/SecondMinimumNodeInBinaryTree.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class SecondMinimumNodeInBinaryTree {
+    public int findSecondMinimumValue(TreeNode root) {
+        return helper(root, root.val);
+    }
+    int helper(TreeNode root, int val) {
+        if (root == null) return - 1;
+        if (root.val > val) return root.val;
+        int left = helper(root.left, val);
+        int right = helper(root.right, val);
+        if (left == -1) return right;
+        if (right == -1) return left;
+        return Math.min(left, right);
+    }
+}
