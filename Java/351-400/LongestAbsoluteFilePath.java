e981225690b5fb529e860572aa25b0fd653bc6f6
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 18:11:01 -0400

    Update longest absolute file path

--- Java/351-400/LongestAbsoluteFilePath.java
+++ Java/351-400/LongestAbsoluteFilePath.java
@@ -1,30 +1,15 @@
 public class LongestAbsoluteFilePath {
     public int lengthLongestPath(String input) {
-        String[] parts = input.split("\n");
-        int maxLen = 0;
-        List<Integer> prefix = new ArrayList<>();
-        prefix.add(0);
-        int len = 0;
-        for (int i = 0; i < parts.length; i++) {
-            int level = getLevel(parts[i]);
-            while (prefix.size() > level) {
-                prefix.remove(prefix.size() - 1);
-            }
-            len = prefix.get(prefix.size() - 1) + parts[i].length() - level + 2;
-            prefix.add(len);
-            if (isFile(parts[i]) && len - 1> maxLen) {
-                maxLen = len - 1;
-            }
+        String[] ps = input.split("\n");
+        Deque<Integer> stack = new ArrayDeque<>();
+        int len = 0, max = 0;
+        for (int i = 0; i < ps.length; i++) {
+            int level = ps[i].lastIndexOf('\t');
+            while (level + 2 <= stack.size()) len -= stack.pop();
+            stack.push(ps[i].length() - level);
+            len += stack.peek();
+            if (ps[i].indexOf('.') != -1) max = Math.max(max, len - 1);
         }
-        return maxLen;
-    }
-    public boolean isFile(String name) {
-        int index = name.indexOf('.');
-        return index != -1 && index < name.length() - 1;
-    }
-    public int getLevel(String name) {
-        int index = 0;
-        while (name.charAt(index) == '\t') index++;
-        return index + 1;
+        return max;
     }
 }
