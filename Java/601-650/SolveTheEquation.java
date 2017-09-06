fb09fb962124022694d84d2323f02ea9f0f65cd4
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 21:36:34 -0400

    Update solve the equation

--- Java/601-650/SolveTheEquation.java
+++ Java/601-650/SolveTheEquation.java
@@ -1,10 +1,8 @@
 public class SolveTheEquation {
     public String solveEquation(String equation) {
         String[] parts = equation.split("=");
-        int[] r1 = solve(parts[0]);
-        int[] r2 = solve(parts[1]);
-        int x = r1[0] - r2[0];
-        int c = r2[1] - r1[1];
+        int[] r1 = solve(parts[0]), r2 = solve(parts[1]);
+        int x = r1[0] - r2[0], c = r2[1] - r1[1];
         if (x == 0) {
             if (c == 0) return "Infinite solutions";
             return "No solution";
@@ -13,19 +11,14 @@ public class SolveTheEquation {
     }
     int[] solve(String s) {
         String[] ps = s.split("[+-]");
-        int len = -1;
-        int x = 0, c = 0;
+        int len = -1, x = 0, c = 0;
         for (String p : ps) {
-            if (p.length() == 0) {
-                len++;
-                continue;
-            }
             int index = p.indexOf('x');
             if (index != -1) {
                 int eff = index == 0 ? 1 : Integer.parseInt(p.substring(0, index));
                 if (len >= 0 && s.charAt(len) == '-') eff = -eff;
                 x += eff;
-            } else {
+            } else if (!p.isEmpty()) {
                 int eff = Integer.parseInt(p);
                 if (len >= 0 && s.charAt(len) == '-') eff = -eff;
                 c += eff;
