4c1f776d76994ef9deebb9d8b3d568c683dd3c9b
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 22:47:12 -0400

    Update verify preorder sequence in BST using O(1) space

--- Java/251-300/VerifyPreorderSequenceInBST.java
+++ Java/251-300/VerifyPreorderSequenceInBST.java
@@ -1,12 +1,11 @@
 public class VerifyPreorderSequenceInBST {
     public boolean verifyPreorder(int[] preorder) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        int low = Integer.MIN_VALUE;
+        int low = Integer.MIN_VALUE, top = -1;
         for (int i = 0; i < preorder.length; i++) {
             int v = preorder[i];
             if (v < low) return false;
-            while (!stack.isEmpty() && stack.peek() < v) low = stack.pop();
-            stack.push(v);
+            while (top >= 0 && preorder[top] < v) low = preorder[top--];
+            preorder[++top] = v;
         }
         return true;
     }
