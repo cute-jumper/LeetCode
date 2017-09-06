04018aa5c07a18c892be4d1fae4c5d14f0b4c7d5
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 22:09:37 -0400

    Update combinations

--- Java/51-100/Combinations.java
+++ Java/51-100/Combinations.java
@@ -1,23 +1,18 @@
 public class Combinations {
     public List<List<Integer>> combine(int n, int k) {
-        return genCombinations(n, k, 0);
-
-    }
-    public List<List<Integer>> genCombinations(int n, int k, int start) {
         List<List<Integer>> res = new ArrayList<>();
-        if (k == 0) {
-            res.add(new ArrayList<Integer>());
-            return res;
-        }
-        for (int i = start; i < n; i++) {
-            List<List<Integer>> combs = genCombinations(n, k - 1, i + 1);
-            for (List<Integer> c : combs) {
-                List<Integer> subRes = new ArrayList<>();
-                subRes.add(i + 1);
-                subRes.addAll(c);
-                res.add(subRes);
-            }
-        }
+        helper(res, new ArrayList<>(), n, 1, k);
         return res;
+    }
+    void helper(List<List<Integer>> res, List<Integer> curr, int n, int start, int k) {
+        if (curr.size() == k) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            curr.add(i);
+            helper(res, curr, n, i + 1, k);
+            curr.remove(curr.size() - 1);
+        }
     }
 }
