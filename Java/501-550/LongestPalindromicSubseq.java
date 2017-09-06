5a311e3c762ae50d5ed8c2dda957f4ce300e8ffc
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 17:45:55 -0400

    Update longest palindromic subsequence

--- Java/501-550/LongestPalindromicSubseq.java
+++ Java/501-550/LongestPalindromicSubseq.java
@@ -1,17 +1,13 @@
 public class LongestPalindromicSubseq {
     public int longestPalindromeSubseq(String s) {
-        if (s.length() == 0) return 0;
+        if (s.isEmpty()) return 0;
         int[][] dp = new int[s.length()][s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            dp[i][i] = 1;
-        }
-        for (int len = 2; len <= s.length(); len++) {
-            for (int i = 0; i + len <= s.length(); i++) {
-                int end = i + len - 1;
-                dp[i][end] = Math.max(dp[i + 1][end], dp[i][end - 1]);
-                if (s.charAt(i) == s.charAt(end)) {
-                    dp[i][end] = Math.max(dp[i][end],
-                                          2 + (len > 2 ? dp[i + 1][end - 1] : 0));
+        for (int i = 0; i < s.length(); i++) dp[i][i] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 0; j + i < s.length(); j++) {
+                dp[j][j + i] = Math.max(dp[j + 1][j + i], dp[j][j + i - 1]);
+                if (s.charAt(j) == s.charAt(j + i)) {
+                    dp[j][j + i] = Math.max(dp[j][j + i], 2 + (i == 1 ? 0 : dp[j + 1][j + i - 1]));
                 }
             }
         }
