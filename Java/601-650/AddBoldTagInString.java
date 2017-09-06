a8dfa23b64371bbbb5a6e63df70f7a244cf9ac4b
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 21:00:29 -0400

    Update add bold tag in string

--- Java/601-650/AddBoldTagInString.java
+++ Java/601-650/AddBoldTagInString.java
@@ -1,49 +1,17 @@
 public class AddBoldTagInString {
-    class Range implements Comparable<Range> {
-        int start;
-        int end;
-        public Range(int s, int e) {
-            start = s;
-            end = e;
-        }
-        public int compareTo(Range r) {
-            return start - r.start;
-        }
-    }
     public String addBoldTag(String s, String[] dict) {
-        List<Range> ranges = new ArrayList<>();
-        for (String d : dict) {
-            int index = 0;
-            while (index < s.length() && index >= 0) {
-                int start = s.indexOf(d, index);
-                if (start == -1) break;
-                ranges.add(new Range(start, start + d.length() - 1));
-                index = start + 1;
+        boolean[] flag = new boolean[s.length()];
+        for (int i = 0, end = 0; i < s.length(); i++) {
+            for (String w : dict) {
+                if (s.startsWith(w, i)) end = Math.max(end, i + w.length());
             }
+            flag[i] = end > i;
         }
-        Collections.sort(ranges);
-        if (ranges.size() == 0) return s;
-        Range curr = ranges.get(0);
         StringBuilder sb = new StringBuilder();
-        if (curr.start > 0) sb.append(s.substring(0, curr.start));
-        int index = 1;
-        for (; index <= ranges.size(); index++) {
-            Range r = null;
-            if (index < ranges.size()) r = ranges.get(index);
-            if (r == null || r.start > curr.end + 1) {
-                sb.append("<b>");
-                sb.append(s.substring(curr.start, curr.end + 1));
-                sb.append("</b>");
-                if (r != null) {
-                    sb.append(s.substring(curr.end + 1, r.start));
-                }
-                if (r != null) curr = r;
-            } else {
-                curr.end = Math.max(curr.end, r.end);
-            }
-        }
-        if (curr.end < s.length() - 1) {
-            sb.append(s.substring(curr.end + 1));
+        for (int i = 0; i < s.length(); i++) {
+            if (flag[i] && (i == 0 || !flag[i - 1])) sb.append("<b>");
+            sb.append(s.charAt(i));
+            if (flag[i] && (i == s.length() - 1 || !flag[i + 1])) sb.append("</b>");
         }
         return sb.toString();
     }
