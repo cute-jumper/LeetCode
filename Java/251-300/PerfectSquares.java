e0f1eded47f85ddfb46f271fa96c0bd00d8c5db2
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 20:14:19 -0400

    Update perfect squares

--- Java/251-300/PerfectSquares.java
+++ Java/251-300/PerfectSquares.java
@@ -1,14 +1,12 @@
 public class PerfectSquares {
     public int numSquares(int n) {
-        int[] nums = new int[n + 1];
-        nums[0] = 0;
+        int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            int minCount = Integer.MAX_VALUE;
+            dp[i] = Integer.MAX_VALUE;
             for (int j = 1; j * j <= i; j++) {
-                minCount = Math.min(minCount, nums[i - j * j] + 1);
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
-            nums[i] = minCount;
         }
-        return nums[n];
+        return dp[n];
     }
 }
