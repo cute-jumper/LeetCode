95c14a3c9ebf898b115610087bae202acf898a04
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 22:02:47 -0400

    Update increasing subsequences

--- Java/451-500/IncreasingSubsequences.java
+++ Java/451-500/IncreasingSubsequences.java
@@ -1,23 +1,17 @@
 public class IncreasingSubsequences {
     public List<List<Integer>> findSubsequences(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        getSeq(res, new ArrayList<>(), nums, 0);
+        helper(res, new ArrayList<>(), nums, 0);
         return res;
     }
-    public void getSeq(List<List<Integer>> res, List<Integer> prefix,
-                                      int[] nums, int start) {
-        if (prefix.size() > 1) {
-            res.add(new ArrayList<Integer>(prefix));
-        }
-        if (start == nums.length) return;
+    void helper(List<List<Integer>> res, List<Integer> curr, int[] nums, int start) {
+        if (curr.size() > 1) res.add(new ArrayList<>(curr));
         Set<Integer> prev = new HashSet<>();
         for (int i = start; i < nums.length; i++) {
-            if (prev.contains(nums[i])) continue;
-            if (prefix.size() == 0 || nums[i] >= prefix.get(prefix.size() - 1)) {
-                prefix.add(nums[i]);
-                getSeq(res, prefix, nums, i + 1);
-                prefix.remove(prefix.size() - 1);
-                prev.add(nums[i]);
+            if (prev.add(nums[i]) && (curr.isEmpty() || nums[i] >= curr.get(curr.size() - 1))) {
+                curr.add(nums[i]);
+                helper(res, curr, nums, i + 1);
+                curr.remove(curr.size() - 1);
             }
         }
     }
