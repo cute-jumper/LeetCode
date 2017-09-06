3fd63b114197481a7e96b4eec615b3614197ba5d
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 16:27:28 -0400

    Update license key formatting

--- Java/451-500/LicenseKeyFormatting.java
+++ Java/451-500/LicenseKeyFormatting.java
@@ -1,31 +1,12 @@
 public class LicenseKeyFormatting {
     public String licenseKeyFormatting(String S, int K) {
-        int len = 0;
-        for (int i = 0; i < S.length(); i++) {
-            if (S.charAt(i) != '-') len++;
-        }
         StringBuilder sb = new StringBuilder();
-        int count = 0;
-        int index = 0;
-        for (; count < len % K && index < S.length(); index++) {
-            if (S.charAt(index) == '-') continue;
-            sb.append(toUpper(S.charAt(index)));
-            count++;
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if (S.charAt(i) != '-') {
+                if (sb.length() % (K + 1) == K) sb.append('-');
+                sb.append(S.charAt(i));
+            }
         }
-        if (len % K != 0) sb.append('-');
-        count = 0;
-        for (int i = index; i < S.length(); i++) {
-            if (S.charAt(i) == '-') continue;
-            sb.append(toUpper(S.charAt(i)));
-            count++;
-            if (count % K == 0)
-                sb.append('-');
-        }
-        if (sb.length() > 1) sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
-    }
-    public char toUpper(char c) {
-        if (c <= 'z' && c >= 'a') return (char)('A' + c - 'a');
-        return c;
+        return sb.reverse().toString().toUpperCase();
     }
 }
