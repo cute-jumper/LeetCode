77a3ffd5cb209b6f4e9f7ed1438b06f09d866452
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-05 23:12:23 -0400

    Update construct binary tree from string

--- Java/501-550/ConstructBTFromString.java
+++ Java/501-550/ConstructBTFromString.java
@@ -9,33 +9,18 @@
  */
 public class ConstructBTFromString {
     public TreeNode str2tree(String s) {
-        return str2tree(s.toCharArray(), 0, s.length() - 1);
-    }
-    TreeNode str2tree(char[] s, int start, int end) {
-        if (start > end) return null;
-        int i = s[start] == '-' ? start + 1 : start;
-        int v = 0;
-        while (i <= end && s[i] >= '0' && s[i] <= '9') {
-            v *= 10;
-            v += s[i] - '0';
-            i++;
+        if (s.isEmpty()) return null;
+        int index = s.indexOf('(');
+        TreeNode root = new TreeNode(Integer.parseInt(index == -1 ? s : s.substring(0, index)));
+        for (int i = index + 1, cnt = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '(') cnt++;
+            else if (s.charAt(i) == ')') cnt--;
+            if (cnt == 0) {
+                root.left = str2tree(s.substring(index + 1, i));
+                if (i + 1 < s.length()) root.right = str2tree(s.substring(i + 2, s.length() - 1));
+                break;
+            }
         }
-        if (s[start] == '-') v = -v;
-        TreeNode root = new TreeNode(v);
-        if (i == end + 1) return root;
-        int j = findEnd(s, i + 1, end);
-        root.left = str2tree(s, i + 1, j - 1);
-        if (j == end) return root;
-        root.right = str2tree(s, j + 2, findEnd(s, j + 2, end) - 1);
         return root;
-    }
-    int findEnd(char[] s, int start, int end) {
-        int pair = 1;
-        for (int j = start + 1; j <= end; j++) {
-            if (s[j] == '(') pair++;
-            else if (s[j] == ')') pair--;
-            if (pair == 0) return j;
-        }
-        return end;
     }
 }
