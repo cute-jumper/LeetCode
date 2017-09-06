ee573fd818c7fd8d3d05b6c18bd02a0dedb77737
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 16:46:42 -0400

    Update group shifted strings

--- Java/201-250/GroupShiftedStrings.java
+++ Java/201-250/GroupShiftedStrings.java
@@ -1,25 +1,19 @@
 public class GroupShiftedStrings {
     public List<List<String>> groupStrings(String[] strings) {
-        Map<String, List<String>> map = new HashMap<>();
-        for (String str : strings) {
-            String s = shift(str);
-            if (!map.containsKey(s)) map.put(s, new ArrayList<>());
-            map.get(s).add(str);
-        }
         List<List<String>> res = new ArrayList<>();
-        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-            res.add(entry.getValue());
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strings) {
+            String k = convert(s);
+            if (!map.containsKey(k)) map.put(k, new ArrayList<>());
+            map.get(k).add(s);
         }
+        res.addAll(map.values());
         return res;
     }
-    public String shift(String s) {
+    String convert(String s) {
         char c = s.charAt(0);
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < s.length(); i++) {
-            int diff = s.charAt(i) - c;
-            if (diff < 0) diff += 26;
-            sb.append((char)diff);
-        }
+        for (int i = 0; i < s.length(); i++) sb.append((char)((s.charAt(i) - c + 26) % 26 + 'a'));
         return sb.toString();
     }
 }
