77ad7cb2eb28a5535bf3f2f6b348ae1f69dc6e9b
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 14:22:36 -0400

    Update binary tree right side view

--- Java/151-200/BinaryTreeRightSideView.java
+++ Java/151-200/BinaryTreeRightSideView.java
@@ -10,18 +10,13 @@
 public class BinaryTreeRightSideView {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        List<TreeNode> current = new ArrayList<>();
-        if (root != null) current.add(root);
-        while (!current.isEmpty()) {
-            List<TreeNode> next = new ArrayList<>();
-            for (int i = 0; i < current.size(); i++) {
-                TreeNode n = current.get(i);
-                if (n.right != null) next.add(n.right);
-                if (n.left != null) next.add(n.left);
-            }
-            res.add(current.get(0).val);
-            current = next;
-        }
+        helper(res, root, 0);
         return res;
+    }
+    void helper(List<Integer> res, TreeNode root, int depth) {
+        if (root == null) return;
+        if (depth == res.size()) res.add(root.val);
+        helper(res, root.right, depth + 1);
+        helper(res, root.left, depth + 1);
     }
 }
