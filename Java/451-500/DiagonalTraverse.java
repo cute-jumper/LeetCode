44920d5d39f458b0918be0d3d25ddaec0e94f790
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 22:22:15 -0400

    Update diagonal traverse

--- Java/451-500/DiagonalTraverse.java
+++ Java/451-500/DiagonalTraverse.java
@@ -1,37 +1,27 @@
 public class DiagonalTraverse {
     public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0)
-            return new int[0];
+        if (matrix.length == 0 || matrix[0].length == 0) return new int[0];
         int height = matrix.length;
         int width = matrix[0].length;
         int[] res = new int[height * width];
-        int[][] dirs = new int[][] { {1, -1}, {-1, 1} };
-        int current = 0;
+        int[][] dirs = new int[][] { {-1, 1}, {1, -1} };
+        int curr = 0;
         int w = 0, h = 0;
-        int index = 0;
-        while (index < res.length) {
-            while (w < width && w >= 0 && h < height && h >= 0) {
-                res[index++] = matrix[h][w];
-                w += dirs[current][0];
-                h += dirs[current][1];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = matrix[h][w];
+            h += dirs[curr][0];
+            w += dirs[curr][1];
+            if (h >= height || h < 0 || w >= width || w < 0) curr ^= 1;
+            if (h >= height) {
+                h = height - 1;
+                w += 2;
             }
-            w -= dirs[current][0];
-            h -= dirs[current][1];
-            if (current == 0) {
-                if (h == 0 && w < width - 1) {
-                    w++;
-                } else {
-                    h++;
-                }
-            } else {
-                if (w == 0 && h < height - 1) {
-                    h++;
-                } else {
-                    w++;
-                }
+            if (w >= width) {
+                w = width - 1;
+                h += 2;
             }
-            current ^= 1;
-            System.out.println(index);
+            if (h < 0) h = 0;
+            if (w < 0) w = 0;
         }
         return res;
     }
