3ba449ac91671d99f674ecfa5d6d4db3a25da1d3
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-07 18:13:25 -0400

    Update count bits

--- Java/301-350/CountBits.java
+++ Java/301-350/CountBits.java
@@ -1,12 +1,7 @@
 public class CountBits {
     public int[] countBits(int num) {
         int[] res = new int[num + 1];
-        res[0] = 0;
-        for (int i = 1; i <= num; i <<= 1) {
-            for (int j = i; j < (i << 1) && j <= num; j++) {
-                res[j] = res[j - i] + 1;
-            }
-        }
+        for (int i = 1; i <= num; i++) res[i] = res[i >> 1] + (i & 1);
         return res;
     }
 }
