8a059f4d4b1312a5a8f6ee0a3dd214fd1df347ac
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 22:03:22 -0400

    Update magical string

--- Java/451-500/MagicalString.java
+++ Java/451-500/MagicalString.java
@@ -1,21 +1,17 @@
 public class MagicalString {
     public int magicalString(int n) {
-        if (n == 0) return 0;
-        if (n < 4) return 1;
+        if (n < 4) return (n + 2) / 3;
         Deque<Integer> queue = new ArrayDeque<>();
-        queue.offerLast(2);
-        int current = 1;
-        int count = 1;
-        int total = 3;
+        queue.offer(2);
+        int total = 3, digit = 1, count = 1;
         while (total < n) {
-            int head = queue.pollFirst();
-            queue.offerLast(current);
-            if (head > 1) queue.offerLast(current);
-            total += head;
-            if (current == 1) count += head;
-            current ^= 3;
+            int h = queue.poll();
+            queue.offer(digit);
+            if (h > 1) queue.offer(digit);
+            if (digit == 1) count += h;
+            total += h;
+            digit ^= 3;
         }
-        if (total > n && current == 2) return count - 1;
-        return count;
+        return total > n && digit == 2 ? count - 1 : count;
     }
 }
