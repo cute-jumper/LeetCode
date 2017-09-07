7f59ed30270c7aeeee14064bb164f73d33d2300f
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 21:12:14 -0400

    Update sort transformed array

--- Java/351-400/SortTransformedArray.java
+++ Java/351-400/SortTransformedArray.java
@@ -1,6 +1,6 @@
 public class SortTransformedArray {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-        boolean inc = a == 0 ? b > 0 : a < 0;
+        boolean inc = a < 0;
         int low = 0, high = nums.length - 1;
         int index = inc ? 0 : nums.length - 1;
         int[] res = new int[nums.length];
