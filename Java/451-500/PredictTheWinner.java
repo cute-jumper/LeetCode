d398bc34ad0c1991e037bc0bbf2b5af2a132306e
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-06 21:43:51 -0400

    Update predict the winner

--- Java/451-500/PredictTheWinner.java
+++ Java/451-500/PredictTheWinner.java
@@ -1,10 +1,10 @@
 public class PredictTheWinner {
     public boolean PredictTheWinner(int[] nums) {
         int[] dp = Arrays.copyOf(nums, nums.length);
-        for (int i = 2; i <= nums.length; i++) {
-            for (int j = dp.length - 1; j >= i - 1; j--) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = dp.length - 1; j >= i; j--) {
                 dp[j] = Math.max(nums[j] - dp[j - 1],
-                                 nums[j - i + 1] - dp[j]);
+                                 nums[j - i] - dp[j]);
             }
         }
         return dp[nums.length - 1] >= 0;
