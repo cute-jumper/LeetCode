71c7dbd33e7ac119bd89de5eef6ff3bff8541a30
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-07 14:49:19 -0400

    Update array nesting

--- Java/551-600/ArrayNesting.java
+++ Java/551-600/ArrayNesting.java
@@ -1,21 +1,14 @@
 public class ArrayNesting {
     public int arrayNesting(int[] nums) {
         int maxSize = 0;
-        int[] size = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            if (size[i] > 0) continue;
-            int index = nums[i];
-            List<Integer> idx = new ArrayList<>();
-            idx.add(i);
-            while (index != i && size[index] == 0) {
-                index = nums[index];
-                idx.add(index);
+            int size = 0;
+            for (int j = i; nums[j] >= 0; size++) {
+                int tmp = nums[j];
+                nums[j] = -1;
+                j = tmp;
             }
-            int s = idx.size() + size[index];
-            for (Integer j : idx) {
-                size[j] = s--;
-            }
-            if (size[i] > maxSize) maxSize = size[i];
+            maxSize = Math.max(maxSize, size);
         }
         return maxSize;
     }
