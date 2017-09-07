7ed1d9bb8873141d2b1353250ce74e7e619c3639
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-07 17:40:31 -0400

    Update queue reconstruction by height

--- Java/401-450/QueueReconstructionByHeight.java
+++ Java/401-450/QueueReconstructionByHeight.java
@@ -1,31 +1,16 @@
 public class QueueReconstructionByHeight {
     public int[][] reconstructQueue(int[][] people) {
-        List<int[]> list = new LinkedList<>();
         Arrays.sort(people, new Comparator<int[]>() {
                 @Override
                 public int compare(int[] a, int[] b) {
-                    if (a[0] > b[0]) return -1;
-                    if (a[0] < b[0]) return 1;
-                    if (a[1] < b[1]) return -1;
-                    if (a[1] > b[1]) return 1;
-                    return 0;
+                    if (a[0] != b[0]) return b[0] - a[0];
+                    return a[1] - b[1];
                 }
             });
-        int index = 0;
-        while (index < people.length) {
-            if (people[index][1] >= list.size()) {
-                list.add(people[index]);
-            } else {
-                list.add(people[index][1], people[index]);
-            }
-            index++;
-        }
-        int[][] res = new int[index][];
-        index = 0;
-        for (int[] p : list) {
-            res[index] = p;
-            index++;
-        }
+        LinkedList<int[]> list = new LinkedList<>();
+        for (int[] p : people) list.add(p[1], p);
+        int[][] res = new int[people.length][];
+        for (int i = 0; i < res.length; i++) res[i] = list.poll();
         return res;
     }
 }
