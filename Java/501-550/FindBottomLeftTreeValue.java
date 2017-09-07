81a38c2388cbc7112dbff6e914e297701ca53d3b
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-07 17:52:53 -0400

    Update find bottom left tree value

--- Java/501-550/FindBottomLeftTreeValue.java
+++ Java/501-550/FindBottomLeftTreeValue.java
@@ -11,17 +11,15 @@ public class FindBottomLeftTreeValue {
     int maxLevel = -1;
     int left = -1;
     public int findBottomLeftValue(TreeNode root) {
+        maxLevel = left = -1;
         getBottomLeft(root, 0);
         return left;
     }
     public void getBottomLeft(TreeNode root, int depth) {
         if (root == null) return;
-        if (root.left == null && root.right == null) {
-            if (depth > maxLevel) {
-                maxLevel = depth;
-                left = root.val;
-            }
-            return;
+        if (depth > maxLevel) {
+            maxLevel = depth;
+            left = root.val;
         }
         getBottomLeft(root.left, depth + 1);
         getBottomLeft(root.right, depth + 1);
