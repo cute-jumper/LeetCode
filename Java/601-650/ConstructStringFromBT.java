376556462cd49d186584d8472697f6c82a8e2130
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 16:23:57 -0400

    Update construct string from binary tree

--- Java/601-650/ConstructStringFromBT.java
+++ Java/601-650/ConstructStringFromBT.java
@@ -10,15 +10,8 @@
 public class ConstructStringFromBT {
     public String tree2str(TreeNode t) {
         if (t == null) return "";
-        if (t.left == null && t.right == null) {
-            return "" + t.val;
-        }
-        if (t.right == null) {
-            return "" + t.val + "(" + tree2str(t.left) + ")";
-        }
-        if (t.left == null) {
-            return "" + t.val + "()(" + tree2str(t.right) + ")";
-        }
-        return "" + t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+        if (t.left == null && t.right == null) return t.val + "";
+        String s = t.val + "(" + tree2str(t.left) + ")";
+        return t.right == null ? s : s + "(" + tree2str(t.right) + ")";
     }
 }
