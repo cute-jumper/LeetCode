751ae2bee1d729f18e6b4931a86465ab8a376e00
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 17:29:58 -0400

    Update logger rate limiter

--- Java/351-400/LoggerRateLimiter.java
+++ Java/351-400/LoggerRateLimiter.java
@@ -9,16 +9,11 @@ public class Logger {
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if (!map.containsKey(message)) {
+        if (!map.containsKey(message) || map.get(message) + 10 <= timestamp) {
             map.put(message, timestamp);
             return true;
-        } else {
-            if (map.get(message) + 10 <= timestamp) {
-                map.put(message, timestamp);
-                return true;
-            }
-            return false;
         }
+        return false;
     }
 }
 
