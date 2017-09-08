03646c35b9f71502984fd59cc2f8287e06fa5867
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 16:40:54 -0400

    Update number of 1 bits

--- Java/151-200/NumberOfOneBits.java
+++ Java/151-200/NumberOfOneBits.java
@@ -3,8 +3,8 @@ public class NumberOfOneBits {
     public int hammingWeight(int n) {
         int count = 0;
         while (n != 0) {
-            count += (n & 1);
-            n >>>= 1;
+            n &= (n - 1);
+            count++;
         }
         return count;
     }
