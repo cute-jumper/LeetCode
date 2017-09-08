a46c6e788955dd90696a24f3931590fa77b1eda4
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 13:32:15 -0400

    Update maximum distance in arrays

--- Java/601-650/MaximumDistanceInArrays.java
+++ Java/601-650/MaximumDistanceInArrays.java
@@ -1,26 +1,13 @@
 public class MaximumDistanceInArrays {
-    public int maxDistance(int[][] arrays) {
-        int len = arrays.length;
-        int[][] mins = new int[len][2];
-        int[][] maxs = new int[len][2];
-        for (int i = 0; i < len; i++) {
-            mins[i][0] = arrays[i][0];
-            maxs[i][0] = arrays[i][arrays[i].length - 1];
-            mins[i][1] = maxs[i][1] = i;
+    public int maxDistance(List<List<Integer>> arrays) {
+        int min = arrays.get(0).get(0), max = arrays.get(0).get(arrays.get(0).size() - 1), dist = 0;
+        for (int i = 1; i < arrays.size(); i++) {
+            int f = arrays.get(i).get(0), e = arrays.get(i).get(arrays.get(i).size() - 1);
+            dist = Math.max(dist, e - min);
+            dist = Math.max(dist, max - f);
+            min = Math.min(f, min);
+            max = Math.max(e, max);
         }
-        Arrays.sort(mins, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
-        Arrays.sort(maxs, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return b[0] - a[0];
-            }
-        });
-        if (maxs[0][1] != mins[0][1]) return maxs[0][0] - mins[0][0];
-        int a = maxs[0][0] - mins[1][0];
-        int b = maxs[1][0] - mins[0][0];
-        return Math.max(a, b);
+        return dist;
     }
 }
