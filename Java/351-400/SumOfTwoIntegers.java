b3469b4d8d9761220c8ed11b3f3be8a2aa6e1996
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 16:38:12 -0400

    Update sum of two integers

--- Java/351-400/SumOfTwoIntegers.java
+++ Java/351-400/SumOfTwoIntegers.java
@@ -1,13 +1,5 @@
 public class SumOfTwoIntegers {
     public int getSum(int a, int b) {
-        int sum = 0;
-        int carry = 0;
-        for (int i = 0; i < 32; i++) {
-            int a1 = a & (1 << i);
-            int b1 = b & (1 << i);
-            sum |= a1 ^ b1 ^ carry;
-            carry = ((a1 & b1) | (a1 & carry) | (b1 & carry)) << 1;
-        }
-        return sum;
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
     }
 }
