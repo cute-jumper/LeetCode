abc4fb543bce78d5af9675fc3f2b0e75b19468df
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 17:23:55 -0400

    Update distribute candies

--- Java/551-600/DistributeCandies.java
+++ Java/551-600/DistributeCandies.java
@@ -2,7 +2,6 @@ public class DistributeCandies {
     public int distributeCandies(int[] candies) {
         Set<Integer> set = new HashSet<>();
         for (int i : candies) set.add(i);
-        if (set.size() > candies.length / 2) return candies.length / 2;
-        return set.size();
+        return Math.min(candies.length / 2, set.size());
     }
 }
