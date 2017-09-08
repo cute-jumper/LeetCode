5a6a668b271d33b427ab4a5c5133b10c138588f0
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 17:44:26 -0400

    Update integer to English words

--- Java/251-300/IntegerToEnglishWords.java
+++ Java/251-300/IntegerToEnglishWords.java
@@ -3,22 +3,17 @@ public class IntegerToEnglishWords {
     final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
     final String[] large = {"", "Thousand", "Million", "Billion"};
     public String numberToWords(int num) {
-
         if (num == 0) return "Zero";
         String s = "";
-        int largeIndex = 0;
-        while (num > 0) {
-            if (num % 1000 > 0)
-                s = convert(num % 1000) + large[largeIndex] + " " + s;
-            num /= 1000;
-            largeIndex++;
+        for (int i = 0; num != 0; i++, num /= 1000) {
+            if (num % 1000 != 0) s = convert(num % 1000) + large[i] + " " + s;
         }
         return s.trim();
     }
-    String convert(int n) {
-        if (n == 0) return "";
-        if (n < 20) return small[n] + " ";
-        if (n < 100) return tens[n / 10] + " " + convert(n % 10);
-        return small[n / 100] + " Hundred " + convert(n % 100);
+    String convert(int i) {
+        if (i == 0) return "";
+        if (i < 20) return small[i] + " ";
+        if (i < 100) return tens[i / 10] + " " + convert(i % 10);
+        return convert(i / 100) + "Hundred " + convert(i % 100);
     }
 }
