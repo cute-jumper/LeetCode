860e2e3dafc1ac16e109b30c226f3cbd59ec8f31
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 15:57:06 -0400

    Update assign cookies

--- Java/451-500/AssignCookies.java
+++ Java/451-500/AssignCookies.java
@@ -2,17 +2,13 @@ public class AssignCookies {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int gIndex = 0;
-        int sIndex = 0;
-        int count = 0;
+        int gIndex = 0, sIndex = 0, count = 0;
         while (gIndex < g.length && sIndex < s.length) {
             if (g[gIndex] <= s[sIndex]) {
                 gIndex++;
-                sIndex++;
                 count++;
-            } else {
-                sIndex++;
             }
+            sIndex++;
         }
         return count;
     }
