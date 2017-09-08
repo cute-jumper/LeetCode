afc50db701113cac9dc0c9bf6baa4d0847f5c2f7
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-08 16:53:18 -0400

    Update max consecutive ones

--- Java/451-500/MaxConsecutiveOnes.java
+++ Java/451-500/MaxConsecutiveOnes.java
@@ -1,12 +1,10 @@
 public class MaxConsecutiveOnes {
     public int findMaxConsecutiveOnes(int[] nums) {
         int max = 0;
-        for (int i = 0, cnt = 0; i <= nums.length; i++) {
+        for (int i = 0, start = 0; i <= nums.length; i++) {
             if (i == nums.length || nums[i] == 0) {
-                max = Math.max(cnt, max);
-                cnt = 0;
-            } else {
-                cnt++;
+                max = Math.max(max, i - start);
+                start = i + 1;
             }
         }
         return max;
