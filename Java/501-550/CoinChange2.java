cfdc517ec17021a47d05f96fffc6e825dd608f70
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-25 18:39:26 -0400

    Update coin change 2

--- Java/501-550/CoinChange2.java
+++ Java/501-550/CoinChange2.java
@@ -1,14 +1,12 @@
 public class CoinChange2 {
     public int change(int amount, int[] coins) {
-        if (coins.length == 0) return amount == 0 ? 1 : 0;
-        int[][] dp = new int[coins.length][amount + 1];
-        for (int i = 0; i < coins.length; i++) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= amount; j++) {
-                if (i > 0) dp[i][j] += dp[i - 1][j];
-                if (j >= coins[i]) dp[i][j] += dp[i][j - coins[i]];
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int c : coins) {
+            for (int i = c; i <= amount; i++) {
+                dp[i] += dp[i - c];
             }
         }
-        return dp[coins.length - 1][amount];
+        return dp[amount];
     }
 }
