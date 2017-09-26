17dbb2791426ea4f2e4430f3846d55db05e39bcf
Junpeng Qiu <qjpchmail@gmail.com>
2017-09-25 20:07:25 -0400

    Update combination sum

--- Java/1-50/CombinationSum.java
+++ Java/1-50/CombinationSum.java
@@ -1,26 +1,18 @@
 public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        return getCombinationSum(candidates, 0, target);
-    }
-    public List<List<Integer>> getCombinationSum(int[] candidates, int low, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        if (target == 0) {
-            res.add(new ArrayList<>());
-            return res;
-        }
-        if (candidates[low] > target) return null;
-        for (int i = low; i < candidates.length; i++) {
-            List<List<Integer>> combs = getCombinationSum(candidates, i, target - candidates[i]);
-            if (combs != null) {
-                for (List<Integer> lst : combs) {
-                    ArrayList<Integer> subRes = new ArrayList<>();
-                    subRes.add(candidates[i]);
-                    subRes.addAll(lst);
-                    res.add(subRes);
-                }
-            }
-        }
+        helper(res, new ArrayList<>(), candidates, 0, target);
         return res;
+    }
+    void helper(List<List<Integer>> res, List<Integer> curr, int[] candidates, int start, int target) {
+        if (target <= 0) {
+            if (target == 0) res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            curr.add(candidates[i]);
+            helper(res, curr, candidates, i, target - candidates[i]);
+            curr.remove(curr.size() - 1);
+        }
     }
 }
