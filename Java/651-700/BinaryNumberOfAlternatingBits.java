fa7b61740df578606958371b732ec4ceebbf616f
Junpeng Qiu <qjpchmail@gmail.com>
2017-10-08 15:49:38 -0400

    Update binary number of alternating bits

--- Java/651-700/BinaryNumberOfAlternatingBits.java
+++ Java/651-700/BinaryNumberOfAlternatingBits.java
@@ -1,10 +1,6 @@
 class BinaryNumberOfAlternatingBits {
     public boolean hasAlternatingBits(int n) {
-        int mask = n & 3;
-        if (mask == 0) return false;
-        for (int i = n; i != 0; i >>= 1, mask = (~mask & 3)) {
-            if ((i & 3) != mask) return false;
-        }
-        return true;
+        long v = (long)n + (n >> 1);
+        return (v & (v + 1)) == 0;
     }
 }
