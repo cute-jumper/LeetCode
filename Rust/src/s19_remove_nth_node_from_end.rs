ee00b089a4edcf0a27d64941245f8c6074071859
Junpeng Qiu <qjpchmail@gmail.com>
2021-06-13 15:31:07 -0700

    Add s19 rust unsafe

--- Rust/src/s19_remove_nth_node_from_end.rs
+++ Rust/src/s19_remove_nth_node_from_end.rs
@@ -4,23 +4,40 @@ struct Solution;
 
 impl Solution {
     pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
-        let mut vhead = Some(Box::new(ListNode::new(0)));
-        vhead.as_mut().unwrap().next = head;
-        let mut fast = &vhead.as_ref().unwrap().next;
-        for _ in 0..n {
-            fast = &fast.as_ref().unwrap().next;
-        }
+        let mut vhead = Some(Box::new(ListNode { val: 0, next: head }));
+        let mut fast = &vhead;
         let mut count = 0;
         while let Some(node) = fast {
             fast = &node.next;
             count += 1;
         }
         let mut slow = &mut vhead;
-        for _ in 0..count {
+        for _ in 0..(count - n - 1) {
             slow = &mut slow.as_mut().unwrap().next;
         }
-        let next = &slow.as_ref().unwrap().next.as_ref().unwrap().next;
-        slow.as_mut().unwrap().next = next.clone();
+        let next = slow.as_mut().unwrap().next.as_mut().unwrap().next.take();
+        slow.as_mut().unwrap().next = next;
+        vhead.unwrap().next
+    }
+
+    pub fn remove_nth_from_end_unsafe(
+        head: Option<Box<ListNode>>,
+        n: i32,
+    ) -> Option<Box<ListNode>> {
+        let mut vhead = Some(Box::new(ListNode { val: 0, next: head }));
+        let mut slow: *mut Option<Box<ListNode>> = &mut vhead;
+        let mut fast = &vhead.as_ref().unwrap().next;
+        for _ in 0..n {
+            fast = &fast.as_ref().unwrap().next;
+        }
+        while let Some(node) = fast {
+            fast = &node.next;
+            slow = unsafe { &mut (*slow).as_mut().unwrap().next as *mut _ };
+        }
+        unsafe {
+            let next = (*slow).as_mut().unwrap().next.as_mut().unwrap().next.take();
+            (*slow).as_mut().unwrap().next = next;
+        }
         vhead.unwrap().next
     }
 }
